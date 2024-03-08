724c7c09d57071f029638526d38c1f6b09c86188
김해인 <khi762@naver.com>
2024-03-08 18:01:58 +0900

    Docs: 체크카드 크롤링 업데이트
    Former-commit-id: c869995936860ab5c1db562a581bb9ee8da82563

--- back/crowling/crowling_card_data.py
+++ back/crowling/crowling_card_data.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fbe07a5815ea04d030ce508322e23d731a564e3116cee6c9fe3a17f66cd6d21c
-size 7416
+oid sha256:2ce2d061add145777cc4ac3457f7402914cb970abddcb2ae31f295adfd04276d
+size 7415
