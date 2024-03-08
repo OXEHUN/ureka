3f400f4323173ded87006306965545bd012eda1c
김해인 <khi762@naver.com>
2024-03-08 17:06:01 +0900

    Docs: 신용카드 크롤링데이터 추가
    Former-commit-id: d913bbb01a940a7f9befb32c7d4efc05f79c5eaa

--- back/crowling/category.py
+++ back/crowling/category.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f02c55e2ed48f5dafc6325fefe19ffadec7d13da1d36bbd48d6b9c6b8553db9
-size 704
+oid sha256:8681f72e50d6612c0bfdc10ba8ce74bbc2fe51e1737d05cd6870fefaae5b3489
+size 701
