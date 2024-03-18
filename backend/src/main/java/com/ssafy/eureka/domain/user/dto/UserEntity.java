2eea96133fc36ee7d60e83ee2740668e8d8c640b
yeongdae <oooo_0@naver.com>
2024-03-18 17:00:23 +0900

    Feat: 회원관리 API
    - 메세지 전송(임시)
    - 회원 확인, 회원 가입, 로그인, 비밀번호 변경, 비밀번호 확인
    - token 재발급, Refresh Token Rotation(RTR)
    
    Change-Id: Ie1b7c92c49ea75fa843bb53317076732bb7a2c62
    
    Former-commit-id: de3a58585c1c47e2e76ec8df158fd4613f6fa1ac

--- backend/src/main/java/com/ssafy/eureka/domain/user/dto/UserEntity.java
+++ backend/src/main/java/com/ssafy/eureka/domain/user/dto/UserEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab679b927c8e32925a7bad358855626aec2a457d3383e543587ce551dac0cc05
-size 1680
+oid sha256:b3e5c92a8ce73478ec5408161534270480015d6a432eb175b660a58aacc2cee1
+size 1757
--- backend/src/main/java/com/ssafy/eureka/domain/user/service/UserService.java
+++ backend/src/main/java/com/ssafy/eureka/domain/user/service/UserService.java
@@ -1,2 +1,3 @@
-package com.ssafy.eureka.domain.user.service;public class UserService {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:c886f9e6ca985124cf9d7421fb4afa5dc069770c1c8182630e2d1b9ef4b0be60
+size 967
