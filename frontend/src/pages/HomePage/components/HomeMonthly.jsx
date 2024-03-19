f44406fa040d47305eb99c114edb333863ecdc39
이승집 <edward626kr@gmail.com>
2024-03-19 17:25:29 +0900

    Feat: Carousel, 페이소비내역
    Change-Id: I4a683f75f3b656617d233eba4a230590c6db3b72
    
    Former-commit-id: 3bf8186daf126754e4c51d1ab1d45d2cfabdfbb1

--- frontend/src/pages/HomePage/components/HomeMonthly.jsx
+++ frontend/src/pages/HomePage/components/HomeMonthly.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c3c588d8ae9a170d6d7c03d7fe9d883157bde3e919b8694276d89ff1b9af3b6
-size 2153
+oid sha256:5e31323abf018ceaaaac100633ad7d71c2c7487654bed46336ef847e3af2c963
+size 2184
--- frontend/src/pages/HomePage/components/HomeOnlyPay.jsx
+++ frontend/src/pages/HomePage/components/HomeOnlyPay.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:78b28fd50a834ea08288334753bb142b21453d85be33e4db2d1dae7e8b86a9e8
-size 1499
+oid sha256:0cfb0a815a73001fc4c5fb7a259df6577c4cdb4bd30030a56dba516c0a315aa4
+size 1530
