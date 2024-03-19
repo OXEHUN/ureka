018a76fd3756d3a2bf3d50c4941acfda8ec58bc7
최용훈 <cyh2607@gmail.com>
2024-03-19 17:26:15 +0900

    Feat: sign up form
    Change-Id: Ic139439e273fd1e8b4dc4ef81b62702b2eab31d1
    
    Former-commit-id: 3374b58ab258c09db47317f4dc759a2874031ed0

--- frontend/App.js
+++ frontend/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85fa3d39e8aff3174279ef21f0982e2d6f58d9328e04fc813484627eef05b135
-size 772
+oid sha256:2dcf952d6c84a4ead869e002d70bbd9c5489d21ebcf7ff994b949c90e72f11a3
+size 1160
