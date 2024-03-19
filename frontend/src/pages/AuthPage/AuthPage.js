4a4b20e962270eb88ab4eaeb1b81aed9ba10fb05
최용훈 <cyh2607@gmail.com>
2024-03-19 17:26:15 +0900

    Feat: sign up form
    Change-Id: Ic139439e273fd1e8b4dc4ef81b62702b2eab31d1
    
    Former-commit-id: 53caeeff118e845814a844e43338e13fbab0f032

--- frontend/src/pages/AuthPage/AuthPage.js
+++ frontend/src/pages/AuthPage/AuthPage.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7ffc6bec78218b71e3b86d60cfad9610d22695d148517012bfdefe8373e4341
-size 1639
+oid sha256:bbc2731c066a2281797349f78f05aed2fd4790386a23484b6b6a83f5aa046479
+size 6144
