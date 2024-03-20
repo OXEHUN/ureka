0a96219b2bfc8b5189b50e456b50b319a6153ff8
kdw <hide4321@naver.com>
2024-03-20 14:52:22 +0900

    Feat: 마이데이터, 결제 토큰 추가
    Change-Id: I0a49074860c3313e426373d6e2313f52bb7c3a58
    
    Former-commit-id: b4dd8cc9bb06b63cb5d13bfa70eb5d722ab69791

---
+++ card/src/main/java/com/ssafy/card/Card/entity/CardEntity.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:94479fd1f18234e310227c2835d93d087dbdb2ef906c75ca0eed982602dccbc5
+size 1513
---
+++ card/src/main/java/com/ssafy/card/User/entity/UserEntity.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:519b6f5d8e8bb60b8e8457c26b368924e4320692a41c7b7c6580a23f4266b424
+size 1285
