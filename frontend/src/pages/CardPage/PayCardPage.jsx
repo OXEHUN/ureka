a75a6394709110d99a732703fba1c5c801800b57
이승집 <edward626kr@gmail.com>
2024-03-20 17:43:23 +0900

    Feat: 카드페이지 구성
    Change-Id: I732d582feb807447a98b1326f99a3fcb8c863819
    
    Former-commit-id: 53a47c052721d544c8b4737c62e9c9e30bcb2ffb

---
+++ frontend/src/pages/CardPage/PayCardPage.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:431cd8f721e9133ea89a733c441349629c955b149f56d34cdd01d7f7e9387662
+size 4555
--- frontend/src/routers/Routers.jsx
+++ frontend/src/routers/Routers.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e7c022e9e45a54d6332f3c451b7d9b70037f92c7cb861b73be247597a913c0d
-size 2135
+oid sha256:4c2c08263e8f8e61ac9403b044228ecd41e90bc265d4e237c1c4700b2c3a52e9
+size 2128
