d7d3d65a29fe246e15b4ac7d76d53bf6d715e2d3
yeongdae <oooo_0@naver.com>
2024-03-20 10:29:09 +0900

    Feat: Card Entity 추가
    - 카드사, 카드 상품, 카드 혜택, 카드 혜택 상세
    - JSON 용 DTO
    
    Change-Id: I6545b0eb56e505405c3e22eaa5f59df589684193
    
    Former-commit-id: b9b2ec57229d32abbffc6431209e2c678b0023fe

--- backend/src/main/java/com/ssafy/eureka/domain/card/dto/CardBenefitDetailEntity.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/dto/CardBenefitDetailEntity.java
@@ -1,16 +1,3 @@
-package com.ssafy.eureka.domain.card.dto;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
-
-@Entity
-@Getter
-@Setter
-@Table(name = "benefit_detail")
-public class CardBenefitDetailEntity {
-    @Id
-    private int cardBenefitDetailId;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:e634cfcab1efcef72514a92a93a21af63fc6dd8f7398b19ed222d67beea02ee2
+size 2552
