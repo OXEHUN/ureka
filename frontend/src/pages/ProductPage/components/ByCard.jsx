468f033c91a9dcb6b5eeea0157847464c7eaee58
oksehun <sehun9803@naver.com>
2024-03-21 17:58:16 +0900

    feat: create recommend product-page
    Change-Id: I288a24b1ad6421e09df288d0813995ceafc2443f
    
    Former-commit-id: 4192c853dcfda1b63c88266a0c8d56f55ab45a92

---
+++ frontend/src/pages/ProductPage/components/ByCard.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:43a52febf1f4409ac6655d1538138363d82b53581d73bde3816e5481caa3bd53
+size 730
---
+++ frontend/src/pages/ProductPage/components/ByCardComponent/ChooseOne.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ea17f2c0e3b72443b708fcae9ad5c7f3f6a6906a3930117ba3f614c773e436d7
+size 5402
