cd7a98883418b4be3e931eaf47f500ec78f494b5
yeongdae <oooo_0@naver.com>
2024-03-22 15:18:53 +0900

    Fix: eureka - mydata 통신 오류 수정
    Change-Id: I09e479623a5f51c626256b5edeb1977df84c4cf8
    
    Former-commit-id: f7a6c1aa3bab240398ab603e81f83c1bdda87ece

--- backend/src/main/java/com/ssafy/eureka/domain/mydata/feign/MyDataFeign.java
+++ backend/src/main/java/com/ssafy/eureka/domain/mydata/feign/MyDataFeign.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7dcba6f85d38c4b0aefaea0fbef068262a5e7b143e5f0a8ea3823d4aa71b3d93
-size 1478
+oid sha256:9f8c30cc47de61d50a14e13e51e0020ce02fa6ecb52e4ba16fb82544cea61d67
+size 1483
