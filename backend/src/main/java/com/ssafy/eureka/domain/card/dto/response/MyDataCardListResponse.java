1034067589f674ebe39e110b140f778e0102bffa
yeongdae <oooo_0@naver.com>
2024-03-22 10:09:50 +0900

    Feat: 유저 카드 관련 Cleint와 Requset, Response Dto 생성
    - Pay, History는 Class만
    
    Change-Id: I4719610a02cebcf3c0c524bac67f6d377babbfea
    
    Former-commit-id: 8549f938f7408d6318304ed352aade20048af265

--- backend/src/main/java/com/ssafy/eureka/domain/card/dto/response/MyDataCardListResponse.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/dto/response/MyDataCardListResponse.java
@@ -1,16 +1,3 @@
-package com.ssafy.eureka.domain.card.dto.response;
-
-import com.ssafy.eureka.domain.card.dto.CardEntity;
-import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class CardListResponse {
-    private List<CardEntity> cardList;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:bbc4bf7b2469789b3b7258258ed98385c1dafa56a314bc04d34547c3dae7fc50
+size 1075
