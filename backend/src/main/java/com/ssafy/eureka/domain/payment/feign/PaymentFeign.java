c2ae51388794575ddb26cc6c89c2c7df0575cc46
yeongdae <oooo_0@naver.com>
2024-03-22 10:59:04 +0900

    Fix: feign localhost -> server url 수정
    Change-Id: I33a7404e827d35a091ee292b8bbb9e9ab46cf8d2
    
    Former-commit-id: 15a7d81a1b8123f7f6d9f502acbe4197b6567ecf

--- backend/src/main/java/com/ssafy/eureka/domain/payment/feign/PaymentFeign.java
+++ backend/src/main/java/com/ssafy/eureka/domain/payment/feign/PaymentFeign.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2aa7dc95cf36cd50d5ae7f4b52e1935bacf728ae2be99b82872966fdf782d67e
-size 1106
+oid sha256:366a401d64f79621bd8dc5dfd039d5ff37717e46a0a947e5e981c20e3529160e
+size 1111
