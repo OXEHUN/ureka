9014e3e883679f0c07f2aa414eb5dee5248ccf3a
yeongdae <oooo_0@naver.com>
2024-03-22 15:21:20 +0900

    Fix: URL 오타 수정
    Change-Id: I7ba0e7c52c1c6afd5cf9318aea1cecf4b944e5d9
    
    Former-commit-id: ffc6b95ae81c8316c61942e52c20e0a932fa663d

--- backend/src/main/java/com/ssafy/eureka/domain/payment/feign/PaymentFeign.java
+++ backend/src/main/java/com/ssafy/eureka/domain/payment/feign/PaymentFeign.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc5e7b1456d1bdbd2ff93bbfca2aac5c7dbf2d66fe0d44099be4cd228b22512e
+oid sha256:366a401d64f79621bd8dc5dfd039d5ff37717e46a0a947e5e981c20e3529160e
 size 1111
