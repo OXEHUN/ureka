ac7173a54eba5c41e72046dca13b02f882f3c7b8
oksehun <sehun9803@naver.com>
2024-03-22 17:40:07 +0900

    feat: product-page
    Change-Id: I31de9a9ba1053044337c6e26321cf1b5e0eb6ba2
    
    Former-commit-id: 92ec2c01f1ef488ee094048fdc3784d8f95a02a2

--- frontend/src/routers/ProductRouters.jsx
+++ frontend/src/routers/ProductRouters.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c14fd58ce77c243afe8a24bf3768fe17a50c339ff1ea01b99ef2eade03f812f
-size 684
+oid sha256:91f7ea7cea307095b6870b7da1d9a262996652f2e452493da43f79c56c69618e
+size 685
