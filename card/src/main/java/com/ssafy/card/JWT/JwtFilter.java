7e7758dad95e0a71e69537273dc3364422839fd3
yeongdae <oooo_0@naver.com>
2024-03-22 10:00:40 +0900

    Fix: Swagger 오류 및 Server - MyData security 에러 해결
    - WHITE LIST 수정, URL 확인, Request 수정 및 기타 사항
    
    Change-Id: I94d4958e11f798f822188eed648aed03fbe4b54f
    
    Former-commit-id: 930cb4bb7b65c25316994ec0430022178e5afc50

--- card/src/main/java/com/ssafy/card/JWT/JwtFilter.java
+++ card/src/main/java/com/ssafy/card/JWT/JwtFilter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b47bea23144af9c03aa680ed907d01afc1f1dc489368f17d76715312e24a90be
-size 2265
+oid sha256:fab2be1cf39eead4e1a4c993585b6ef82f5c431a83f6839c106a7740e702b7c1
+size 2322
