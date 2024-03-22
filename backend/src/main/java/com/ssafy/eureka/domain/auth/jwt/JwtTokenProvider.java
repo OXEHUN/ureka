c47d9bdf85531b5d342eb38dc0cc4936d154a1ab
yeongdae <oooo_0@naver.com>
2024-03-22 10:59:04 +0900

    Fix: feign localhost -> server url 수정
    Change-Id: I33a7404e827d35a091ee292b8bbb9e9ab46cf8d2
    
    Former-commit-id: 88eae62f017f112fbc0669739d8c13e56aaa72e2

--- backend/src/main/java/com/ssafy/eureka/domain/auth/jwt/JwtTokenProvider.java
+++ backend/src/main/java/com/ssafy/eureka/domain/auth/jwt/JwtTokenProvider.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07295e516ec636d38893b9cc1ad0c1f42aa2e67b9e1ae76c3ebfe25da46c31a4
-size 3985
+oid sha256:8891aee1272a0645ee028fd849c8de68dff764683bb9751fceba9860e2d34940
+size 4053
