13ff0f75768491d345f9f58513a5bae360cd5dc0
oksehun <sehun9803@naver.com>
2024-03-22 09:53:27 +0900

    just check
    Change-Id: Ia568d5d7691b20b53574013158533dfbbf7d6a23
    
    Former-commit-id: 60d54a6afb5e896146d1c9b4756d7f403a4861b1

--- frontend/src/pages/ProductPage/components/ByCardComponent/CardList.jsx
+++ frontend/src/pages/ProductPage/components/ByCardComponent/CardList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac69477feea9382d50ca1437ed16ba1a8d584b1d50a4e168044d02d77035d924
-size 1674
+oid sha256:51acb2106c53858ec7a8bbb2846a0f036995e483bc1ce2476fc2c6b663595217
+size 1633
