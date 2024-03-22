0b952672351849b266006bece52a809dba42730d
yeongdae <oooo_0@naver.com>
2024-03-22 15:21:20 +0900

    Fix: URL 오타 수정
    Change-Id: I7ba0e7c52c1c6afd5cf9318aea1cecf4b944e5d9
    
    Former-commit-id: 57621a0fc194e19dfa416cef87020c198d0cf023

--- backend/src/main/java/com/ssafy/eureka/domain/mydata/feign/MyDataFeign.java
+++ backend/src/main/java/com/ssafy/eureka/domain/mydata/feign/MyDataFeign.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f8c30cc47de61d50a14e13e51e0020ce02fa6ecb52e4ba16fb82544cea61d67
+oid sha256:f050e59286b21e763da38fdcf8249cf144353a3d1dd4b3967dc2b63ecdf70c41
 size 1483
