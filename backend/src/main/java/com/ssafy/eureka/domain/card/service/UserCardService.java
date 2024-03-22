9f35a820598d45ac659ddf36c133ee431ce5387c
yeongdae <oooo_0@naver.com>
2024-03-22 10:08:35 +0900

    Feat: 서버 / 유저 카드 관련 API 구현
    - MyData에 보유 카드 조회하기
    - 서버에 보유 카드로 등록하기
    - 결제 카드로 등록하기
    - 삭제의 경우 method만 만듬 ( 내부 로직 X ) / 어떻게 할지 생각해봐야 함.
    
    Change-Id: I1d8692b6f5a6312dfbd9c9c41afe29e223192ba9
    
    Former-commit-id: 5deb47320e3d17ac2556f436d05fbbb140bd9a91

--- backend/src/main/java/com/ssafy/eureka/domain/card/service/UserCardService.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/service/UserCardService.java
@@ -1,5 +1,3 @@
-package com.ssafy.eureka.domain.card.service;
-
-public interface UserCardService {
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:26435449efa67ae774423387b4edb189ab0d5602ec478052e988c63d359bd2fb
+size 992
