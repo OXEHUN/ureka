9327cc7709dccf52ae303e87f9a6ed6176f94d2b
yeongdae <oooo_0@naver.com>
2024-03-25 14:02:12 +0900

    Feat: MyDataCardListResponse cardCompanyId 추가
    Change-Id: I0fa0e291003500ca769dc577c61325d5bced9593
    
    Former-commit-id: 0534f37680e8018d6f548448fd8fe6ba3b631765

--- backend/src/main/java/com/ssafy/eureka/domain/card/service/UserCardServiceImpl.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/service/UserCardServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec0ea4cd6270ae1ca6cc88c1a746c51d1befb004181faf921efe2c570414b906
-size 8128
+oid sha256:19c3aa218dd2697a00f173ae4265ea81f1d8bbb33a1d9d656fed80136130bf6d
+size 8134
