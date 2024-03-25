5ea277758848fc38472151a68af8805ff8b0e460
yeongdae <oooo_0@naver.com>
2024-03-25 10:05:33 +0900

    Feat: MyData, Card ApiResponse 수정
    Change-Id: I54c4b21fa47bde0109048bd83c1226d5aea62343
    
    Former-commit-id: 07bec37dab9af9b14b8c689c916614671e47d32c

--- backend/src/main/java/com/ssafy/eureka/common/response/ResponseCode.java
+++ backend/src/main/java/com/ssafy/eureka/common/response/ResponseCode.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf6d4ea43ac91f97e7dc24c6e0c670ebac0e3035cd2b5b3e16738d157e1f4b59
-size 1299
+oid sha256:327c52c3f12564fd37491ae38488297bbd37f5898e5a6358fe8e9a9846d851ca
+size 1363
