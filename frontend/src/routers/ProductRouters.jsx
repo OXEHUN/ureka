f177226802e7f0122a3e7f7ec50688282b9523e5
oksehun <sehun9803@naver.com>
2024-03-25 17:49:11 +0900

    feat: create FitYourConsumption page
    Change-Id: Idac8e98c69c1fef59aa20015f66c7f9d51d06b1a
    
    Former-commit-id: 939fae3b02f54ca4a9f3e34f111f3b46ed8de183

--- frontend/src/routers/ProductRouters.jsx
+++ frontend/src/routers/ProductRouters.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e99a6e19e810d4361e7d0924a9cc2549d6ca3e336d12de1586b68c3d1501cd19
-size 1023
+oid sha256:a341d5745cf2831f54d2d04c88320c411d547946ee1c35cd5605f80e42cf1976
+size 1217
