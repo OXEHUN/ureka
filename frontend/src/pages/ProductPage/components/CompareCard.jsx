427180451ebc9df81b9912faa83817452be1008f
oksehun <sehun9803@naver.com>
2024-03-25 15:45:24 +0900

    feat: create compareCard
    Change-Id: I05351480c654b1eb0472a103ff71142ab0578110
    
    Former-commit-id: bef13e97f6909a57fb21e3e14b5c3e956cace127

---
+++ frontend/src/pages/ProductPage/components/CompareCard.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2e893fa95cc97e834b7d43537e75efad74393cea251af4c91df8ec4af7e9bef5
+size 9733
--- frontend/src/pages/StatisticsPage/components/MyAkaList.jsx
+++ frontend/src/pages/StatisticsPage/components/MyAkaList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be5cefb69c0d7d68aa113bbb02f47bbe2d27036d875055454ab8f81af5991876
-size 3091
+oid sha256:5a0055f38722a521bea7815c7406399319f6752f7412aae13474ddbcc0bdbe23
+size 3092
