73b0cb0e1668dd31bb6d2e5712d40f576273d03a
oksehun <sehun9803@naver.com>
2024-03-26 11:58:02 +0900

    Feat: create fityourconsumption
    Change-Id: I523a89cad143a7cc12fc5d1e793d6430108962fd
    
    Former-commit-id: ed248f34a271eb3e68d3855463e57ab4d1133178

--- frontend/src/pages/ProductPage/components/FitYourConsumption.jsx
+++ frontend/src/pages/ProductPage/components/FitYourConsumption.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c449f32b3f1b49c0dd4cb6de2e321a13d908e730b111369eef323ef8ad6790b
-size 1998
+oid sha256:84f6df18c361d12466b186c27fe4ddde3b42c55a13b587a8dd250b20959def17
+size 5644
