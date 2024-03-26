ad8f3283a867a34a83bfab5b82c512f584c9642a
최용훈 <cyh2607@gmail.com>
2024-03-26 15:45:30 +0900

    Fix:회원가입 데이터 처리 및 토큰저장
    Change-Id: Iabf935d01d23f53b0ae7ecb38b57242a2d1dfe05
    
    Former-commit-id: f0fb2e496df609cd21f6b067879960d25684664e

--- frontend/src/pages/SignUpPage/PasswordConfirmPage.jsx
+++ frontend/src/pages/SignUpPage/PasswordConfirmPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e73d0ad4af74479a4a28ced4c19f26c196a40818ef698a4e6fa69bdf775155bc
-size 7212
+oid sha256:0182283ca9e1b764a4567c2e0775ba63153d697ff7f9710b06ffb5ce73530d14
+size 7207
--- frontend/src/pages/SignUpPage/SignupPage.jsx
+++ frontend/src/pages/SignUpPage/SignupPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45a4bfbc813bbb5626cd9e53f2a73ecd08a92f4aa34a9bb98fead4686ddb0a7f
-size 12839
+oid sha256:f0d3f5a40f9e0cb7aa3ee565caef3b27d8caa0d55bb39cc78b7a660d8d8734bd
+size 12834
