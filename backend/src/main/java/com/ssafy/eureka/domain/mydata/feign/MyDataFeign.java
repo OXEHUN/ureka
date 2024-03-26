ecd7adafcfd8d2d81afbb1dd228fb13822d35da6
yeongdae <oooo_0@naver.com>
2024-03-26 15:21:12 +0900

    Feat: feign url error fix
    Change-Id: If644f06b4cd4f068d2f72fc292ddfe15e43ef1fe
    
    Former-commit-id: d064762d83415156d8450b105fe1461a231dcf06

--- backend/src/main/java/com/ssafy/eureka/domain/mydata/feign/MyDataFeign.java
+++ backend/src/main/java/com/ssafy/eureka/domain/mydata/feign/MyDataFeign.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c2b9816460403cecefdd90a5a712c004d146b6999cfbbc04fde7f0d03422026
-size 1587
+oid sha256:67c05e0632d8f945d0514648fed666ea60d75bd941866496f908105e0230f35d
+size 1583
