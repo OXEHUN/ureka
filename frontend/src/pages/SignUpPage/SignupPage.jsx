454b2b0f133d18b773e40555209bda1672bff83a
최용훈 <cyh2607@gmail.com>
2024-03-26 16:21:43 +0900

    Fix:회원가입 로직 및 토큰저장관리
    Change-Id: I41c4136881851e93903f02750563daca69b1a7a3
    
    Former-commit-id: 3ddb5ec3d9930bdc4687871af9fa2a062495cd03

--- frontend/src/pages/SignUpPage/SignupPage.jsx
+++ frontend/src/pages/SignUpPage/SignupPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0d3f5a40f9e0cb7aa3ee565caef3b27d8caa0d55bb39cc78b7a660d8d8734bd
-size 12834
+oid sha256:0613b8e1251f2213b5fa383155697cb43bfc126105246b43ea82cba3d2bfccba
+size 12648
