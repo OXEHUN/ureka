82d858a298dccfea389a8aaba4ccb00eb7897c31
kdw <hide4321@naver.com>
2024-03-27 00:45:28 +0900

    Fix: PayHistory Logic Fix
    Change-Id: Ie6ef6d93194905b06cff4d069acb56598e64a2a9
    
    Former-commit-id: ff4780e679debe1d87faef2cfbb28910b78e6002

--- backend/src/main/java/com/ssafy/eureka/domain/mydata/feign/MyDataFeign.java
+++ backend/src/main/java/com/ssafy/eureka/domain/mydata/feign/MyDataFeign.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3d3f9b1e48322b4338df4f7c03de0725c289dfc791ef2bf5cf8299089598b30
-size 1598
+oid sha256:638203e45dd1abf3f8ed120a962cdc1cecad3e0a7104f35adf0be8793a45a78c
+size 1529
