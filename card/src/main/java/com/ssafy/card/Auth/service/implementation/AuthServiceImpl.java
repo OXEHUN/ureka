992a33bafdd2ed4045ac713171b01ffc389a594a
kdw <hide4321@naver.com>
2024-03-26 12:17:03 +0900

    Fix: PayTokenResponse restore
    Change-Id: Ibc27038dcfcb238b03613a4d1e0ea3aa7e87765d
    
    Former-commit-id: f8002689b950bb15479557a63ce4cdce79a268aa

--- card/src/main/java/com/ssafy/card/Auth/service/implementation/AuthServiceImpl.java
+++ card/src/main/java/com/ssafy/card/Auth/service/implementation/AuthServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86dbfa5cac469eb4ed3b447669478574a836f3f842825ee9107d69a1a25bc8fd
-size 4207
+oid sha256:633ff08adb6603c137d824c9c317762743132b94b4aa937b6acf1b48ae118a86
+size 4333
