c7f1ce828a907e9b40e52a04c4205d675a8998b9
kdw <hide4321@naver.com>
2024-03-27 00:27:15 +0900

    Fix: Url Mapping Fix
    Change-Id: Icda1d86e70069374eb824cd30bcfb467127518b8
    
    Former-commit-id: c8ed58d2c34c7ecb342054466cc5e9e1d390903d

--- backend/src/main/java/com/ssafy/eureka/domain/mydata/feign/MyDataFeign.java
+++ backend/src/main/java/com/ssafy/eureka/domain/mydata/feign/MyDataFeign.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3d3f9b1e48322b4338df4f7c03de0725c289dfc791ef2bf5cf8299089598b30
+oid sha256:09fcfb100d64106d957ea8fe26a84b8768263472e0431b720e7ba49ca9c73372
 size 1598
