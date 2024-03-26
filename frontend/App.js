bcf3db129b97933734c244844787bc094f2a94ec
이승집 <edward626kr@gmail.com>
2024-03-26 14:59:44 +0900

    Fix: App.js 주석 삭제
    Change-Id: Ia7afdb281abf928c5971383d8ffd39616231b274
    
    Former-commit-id: ddff6f9a303cda86fa484b5806837c23939fcdd0

--- frontend/App.js
+++ frontend/App.js
@@ -1,3 +1,17 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:dab0aba6d4cf12d7375baa02fae2dd7bdf47627fc9b746534dd0b00be852dd19
-size 909
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { store } from './src/stores/store';
+import { Provider } from 'react-redux'
+import AuthRouters from './src/routers/AuthRouters'
+
+
+export default function App () {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <StatusBar />
+        <AuthRouters />
+      </NavigationContainer>
+    </Provider>
+  );
+}
