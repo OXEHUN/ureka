8e0e14262ea7eba0ee9440398f9430be2fd065a9
yeongdae <oooo_0@naver.com>
2024-03-26 09:38:50 +0900

    Fix: 서버에 보유 카드 등록 시 중복 문제 수정
    Change-Id: Id52cf92b9ec939df0104639c9de70e1f96d77dfb
    
    Former-commit-id: 723e34ae62f9c354ff7acb2984cb1dc2172d9fc4

--- backend/src/main/java/com/ssafy/eureka/domain/pay/dto/PayHistoryEntity.java
+++ backend/src/main/java/com/ssafy/eureka/domain/pay/dto/PayHistoryEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f22e906f892d10a1c9eb535da0ab1e15b155e18ea07df660d356a4124b2ba43
-size 990
+oid sha256:1b636d7531ef3cd9617b430522de6ed102c4586745ef1e179209a75a65929097
+size 1132
