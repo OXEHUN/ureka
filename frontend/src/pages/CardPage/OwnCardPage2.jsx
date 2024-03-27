997616973f816bf8196409100724446961a43caa
이승집 <edward626kr@naver.com>
2024-03-28 00:01:33 +0900

    Feat: 카드 이미지 방향 맞추기
    Change-Id: I1f040cf2c130852bf8fcabc430667317cf2bbeb3
    
    Former-commit-id: 354063dc49e40b4f72600e8aaf9df49394233091

--- frontend/src/pages/CardPage/OwnCardPage2.jsx
+++ frontend/src/pages/CardPage/OwnCardPage2.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3013cc76b65ac20317bfde4fbd1649f0bc935677409fe526486118763b1c5d6
-size 4538
+oid sha256:8607391f7b965f71b832b6290ba4e96cb3d3ca19d9aabfa2d5e89c7cccc60931
+size 4505
