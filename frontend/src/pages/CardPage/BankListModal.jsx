019d31646e8112f55c24ff8cb75261cf27521ecb
oksehun <sehun9803@naver.com>
2024-03-27 18:01:11 +0900

    Fix: png
    Change-Id: Ic51184a0329e9ae82316ce5588d8767daf730c03
    
    Former-commit-id: b90dfadfd1891c627079a0485c63676b7e351b80

--- frontend/src/pages/CardPage/BankListModal.jsx
+++ frontend/src/pages/CardPage/BankListModal.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb48a79f488a9a7c3203a59176b169a2d152358e8bcd1bc5792d6ce0b0172196
-size 6607
+oid sha256:281f0b69c4f9bdd0d3f5fcd20cfadc66b41e12b178226293855e2d928f5ac7b7
+size 7218
