1a44eee747afc687f0a134b510ec24736c45a40c
kdw <hide4321@naver.com>
2024-03-27 23:05:23 +0900

    Fix: 보유 카드 조회 카드 이미지 방향 수정
    Change-Id: I55464115b3a9f00f5e604f544c4956414b488ef0
    
    Former-commit-id: 0a3ff0e0ff73186eb8322fab3a8f9aab300a06d8

--- backend/src/main/java/com/ssafy/eureka/domain/card/service/UserCardServiceImpl.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/service/UserCardServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f08e922163de59a5b08ca9805995437bc83449285c19c00ebfe45aa8714249d
-size 11995
+oid sha256:7168768f8d7a73dc597aea0e82874d1fc9008af44beb9efaf1a0ee8d8fc3d915
+size 12072
