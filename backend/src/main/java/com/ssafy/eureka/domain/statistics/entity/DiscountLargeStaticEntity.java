75b6d2e085fede5bedb49b1199ef142d8e123319
김해인 <khi762@naver.com>
2024-03-27 17:44:18 +0900

    Feat: 총 소비, 할인 금액 조회 api
    Change-Id: Id408273a304626f4a12ce4719a61ef616029edd0
    
    Former-commit-id: 1ef1cc8cec53b4c0158b5e37ff01d2cd13a539cd

---
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/entity/DiscountLargeStaticEntity.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f0513ef00c8c8d99f794e26417da64fb1be7ca962719c2576915682227544135
+size 600
---
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/entity/DiscountSmallStaticEntity.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:95d2c146801ea918b0aebe30860e345c14a6ad915a28bd39dccd60bd1ceb7610
+size 570
