918e8f50390401d72cc4d5daea038c23be317bc2
yeongdae <oooo_0@naver.com>
2024-03-27 23:19:39 +0900

    Feat: sql문 수정 및 entity db에 맞춰서 설정
    - 이제 JPA  auto-ddl설정은 none에서 바꾸지 말기~
    
    Change-Id: I39be502b64ce414d54002cfee843b8a87de3c3d8
    
    Former-commit-id: 6db178971a3958f20763d1d8353aa06afe1fc792

--- backend/src/main/java/com/ssafy/eureka/domain/card/dto/CardBenefitEntity.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/dto/CardBenefitEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59256be1890e57c788e702d3a86bac67eb161ca2291e4ea55cae331a1c823525
-size 1184
+oid sha256:a8d02fbbf312d7b2d56c6b9b3a60ccb6f8e930164f50721306124f294a0f3fdb
+size 1210
--- backend/src/main/java/com/ssafy/eureka/domain/category/dto/SmallCategoryEntity.java
+++ backend/src/main/java/com/ssafy/eureka/domain/category/dto/SmallCategoryEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25cf6f407dbfc219d001f9d888359c7cc982405b5a14275fa439c8ee83023f4a
-size 918
+oid sha256:ce9cd0e794e5cf1463bcace0280ed3bc8ea84ebbc9a2bc7966c2fddffa027508
+size 979
