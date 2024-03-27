0af7ec3a5201c3ba61e14caadda04955f5dff8eb
yeongdae <oooo_0@naver.com>
2024-03-27 21:06:41 +0900

    Feat: SQL 작성 및 카드 상품 더미 데이터 추가
    Change-Id: I9f88f2b2c91de9decd10abacc9cc0e0801bfd992
    
    Former-commit-id: 030f299db6ee240ca61727a71b3f7debf1296bdc

--- document/eureka_db_dummy.sql
+++ document/eureka_db_dummy.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47e348f22dabebc6fb23d1a99127e722aeff4bafc316a6aea19dcc6ac1823f37
-size 1740
+oid sha256:bb9d5d719dae8d00063e9d92aa7d39e525793f2d54dbcd613e4b1eabf7b2b611
+size 2529
