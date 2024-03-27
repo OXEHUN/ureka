d62cb4dbc55370c3b42f6480034948aa1f439a31
김해인 <khi762@naver.com>
2024-03-27 17:44:18 +0900

    Feat: 총 소비, 할인 금액 조회 api
    Change-Id: Id408273a304626f4a12ce4719a61ef616029edd0
    
    Former-commit-id: bfbc06a4039bc86dd0aabc8527e58f88bc2567b0

--- backend/src/main/java/com/ssafy/eureka/domain/statistics/repository/ConsumptionStaticRepository.java
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/repository/ConsumptionStaticRepository.java
@@ -1,8 +1,3 @@
-package com.ssafy.eureka.domain.statistics.repository;
-
-import com.ssafy.eureka.domain.statistics.entity.ConsumptionStaticEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConsumptionStaticRepository extends JpaRepository<ConsumptionStaticEntity, String> {
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:45715e42430c162fa60ff851f116d905f39b5183b98a3167adf3c60a0cdc3bcd
+size 858
---
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/service/StatisticServiceImpl.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d3d489797772cb27257c3e85de71b96cc14a291b1c5ed352a672246c9dcb4750
+size 2639
