4a47c97d1f48d0978bb76a532cf9621055b7ca87
kdw <hide4321@naver.com>
2024-03-27 16:38:53 +0900

    Fix: 유저 보유카드 조회 수정, 카드사, 카테고리별 조회 방향 추가
    Change-Id: Ie40902c36201032f04f67d52cc8d67ed063683b5
    
    Former-commit-id: d7280ea25f80b49caea6c5fd0b9819147c5dea7b

--- backend/src/main/java/com/ssafy/eureka/domain/card/dto/response/OwnUserCardResponse.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/dto/response/OwnUserCardResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea658036b7749c850765c55eb716edb152d643904f8f9fd1aad96d1a213725b5
-size 1967
+oid sha256:f4491d790e9bdb588f40bfc0fd5f4f05c055668d3988e3c4dbc7e5b9c9292097
+size 2040
--- backend/src/main/java/com/ssafy/eureka/domain/card/service/UserCardServiceImpl.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/service/UserCardServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:968e683287e3328e7ca24c2930dcfa14114f3571b371aaf45fad86eab4069de7
-size 12867
+oid sha256:33f2f681ccd6f3fa83cc98ce28b0c5455069a19f44513081b8e44c1649223506
+size 12977
