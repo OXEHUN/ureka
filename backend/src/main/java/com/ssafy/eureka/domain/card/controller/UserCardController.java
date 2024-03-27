ea3923793bbe1214947bb5fc811f0557e358cb15
kdw <hide4321@naver.com>
2024-03-27 17:34:01 +0900

    Fix: 카드 상품 상세 조회 -> 카드 혜택 테이블만 보이게 싹 수정
    Change-Id: Ibf2eb7ff24aaf69d4ae499b37e5a5ca143a415f0
    
    Former-commit-id: 310d3eaddce62d1aca8d30be818a26cc150989e8

--- backend/src/main/java/com/ssafy/eureka/domain/card/controller/UserCardController.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/controller/UserCardController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:721a73f1eaa30841b842b56baefb8feb896a86e10818534caa9bf1d99885ab5e
-size 4184
+oid sha256:ebeec3f8bce96f9dc91e3428207e05f39cb1bf4fa5e0f1dc76feda9b7c742f23
+size 4219
