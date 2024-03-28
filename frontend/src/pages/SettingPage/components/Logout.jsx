1557a056d78a125cba9c7152562a6fd31f414b01
최용훈 <cyh2607@gmail.com>
2024-03-28 14:01:22 +0900

    Feat:세팅페이지, 지문인증
    Change-Id: I9d2e49b56f9c5c54bb8bea05aa178dadffcf444b
    
    Former-commit-id: 592b9dc57de4bc55f2daae9406005f5006ae9b7d

---
+++ frontend/src/pages/SettingPage/components/Logout.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:41a48e8b2f7dbf1ec7f5b778281066c7c51aadcc649406dcee40f33954229ae5
+size 1645
--- frontend/src/stores/TokenUtils.jsx
+++ frontend/src/stores/TokenUtils.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96c40fc8086163e2f53becb978f391b4619122df6017cc24330d9d284a9e80db
-size 1394
+oid sha256:cf4c012e8529e41294782f177f3eae4bdc8c98e3ef77c25e603d8165e60339d6
+size 1390
