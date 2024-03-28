08e80209bb2c48313f6ebb6f7cf2c1b9400bae59
yeongdae <oooo_0@naver.com>
2024-03-28 22:39:45 +0900

    Feat: 거래내역 조회 오류 수정
    Change-Id: I01c75ece135aeb44f20403f781a4482fdaa9997d
    
    Former-commit-id: bbb36ff1b32e9249df73de2bf455fe0279eedfe3

--- document/오싸피 거래내역.sql
+++ document/오싸피 거래내역.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21471e389f85ab645fc72586a098ce5eac93254a1bbafcbd95bf6382bb80933a
-size 94058
+oid sha256:e60a639b3935c4dc0d7a724683f3797056ae7376429a50b961b01514631b6dfe
+size 94353
