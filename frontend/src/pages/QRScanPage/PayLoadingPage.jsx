eab2db4530b94882a4eb4f37e684fe7f98f1a8fb
김해인 <khi762@naver.com>
2024-03-28 15:22:24 +0900

    Fix: 빌드오류 수정 - 에셋 이름 변경
    Change-Id: Ie6d9cd3ce94139466003ba2f6295865006baeda0
    
    Former-commit-id: 6132316367a588a79ca05a72eec4416b2aa0b722

--- frontend/src/pages/QRScanPage/PayLoadingPage.jsx
+++ frontend/src/pages/QRScanPage/PayLoadingPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fbf895b82b6d4fd60a97400ba802ac04b1181f186c65efe2120885cb6489e27e
-size 1477
+oid sha256:6a8fa4b536ddf9feeb0de560cf4868137fbcc1936d8f245aed8129922ceade17
+size 1480
