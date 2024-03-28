87f166e37bda8040f0ebbb7b3622977abe7f3656
yeongdae <oooo_0@naver.com>
2024-03-28 22:13:41 +0900

    Feat: 카드 결제 내역 조회 수정
    Change-Id: I1bf136b4df8ebe6de86b61a244be4c2f72e58f24
    
    Former-commit-id: fa5b4199d8b34498ba82a4623e4e05a41a6ebd78

--- backend/src/main/java/com/ssafy/eureka/domain/card/service/UserCardServiceImpl.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/service/UserCardServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8c557dc16d7a107cc6fcabec4fa2bec7f01767f2473e4e3f4e2342825db3c70
-size 13296
+oid sha256:d74ef2004b3412aabd31a589fa3fea5b41bf5551863510f3f40e3cfc2d541bcc
+size 13217
--- backend/src/main/java/com/ssafy/eureka/domain/mydata/dto/response/MyDataCardHistoryResponse.java
+++ backend/src/main/java/com/ssafy/eureka/domain/mydata/dto/response/MyDataCardHistoryResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df70d221e4a8bc657a8917a9a784a8574553643388841e97b6e37ba0cfc1457e
+oid sha256:48e5910ef503e7a9762e5bb2373ca7b7665ae43a3cf852821cae76dffc7474e2
 size 877
