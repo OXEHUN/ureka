1fcab8c9e22ab3dbfe319db821ad2d3df7c64406
yeongdae <oooo_0@naver.com>
2024-03-28 23:47:14 +0900

    Fix: 더미데이터 수정
    Change-Id: Ideff7b1e2b3f0a85d55d21f755dfa8732ddbafb8
    
    Former-commit-id: 0046f817148eec688a70966380ef21aa285f9135

--- document/card_db_dummy_2.sql
+++ document/card_db_dummy_2.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c675324ba6739e0d57dd59ec6a9666ec2662bde7268aea3523271e18a1d0f0e5
-size 4132
+oid sha256:3770980aa3e2fb95930f7ed32dbf5b75892ddfef0dcb819d4307faf687d1af9b
+size 4253
--- document/eureka_db.sql
+++ document/eureka_db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c9b12afb1c600510af30ebf420ce1a4cf979703a71ca68f2ba3ad8276ca2870
-size 15834
+oid sha256:c06cb319b23b467d94a55877646478c0463164f4ddee4b0f4e4e165cdd38c61e
+size 15836
