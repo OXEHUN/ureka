eda34f7cb4c611984391532ec9d59f643aa79044
yeongdae <oooo_0@naver.com>
2024-03-28 11:10:19 +0900

    Feat: 카드 상품 더미 데이터 수정 및, sql문 수정
    Change-Id: I8dc93c74e126cb33366ef0ef1b6001a59a30f68f
    
    Former-commit-id: 1cefbc2b9d5d4b0f2d4b9e0a4d53a400c29b4f55

--- document/card_db.sql
+++ document/card_db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:191b033aa9229edb81ce8be458c92846612da3e999811cf0a2122a5be8d67bac
-size 5570
+oid sha256:03b125236d4c0cc72e38092a1eb3713f0b0e81a073784c79989d528f2213fa48
+size 5575
--- document/card_db_dummy.sql
+++ document/card_db_dummy.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb9edbd9169649d0d4474b93c57594b8bf29887ae022651888d882b4a8bb7504
-size 3881
+oid sha256:9bda6f7a932bf0ccf1f31bfb7c040978f76617b8277397b8f19da864adfa9fe8
+size 751847
