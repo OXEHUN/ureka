01be0168f5fcd2c1b2ca2d967492fc941e1be636
yeongdae <oooo_0@naver.com>
2024-03-28 11:42:00 +0900

    Fix: DB 컬럼 수정
    Change-Id: I5844ae22f3279c71d1f2de5508846c33ba394953
    
    Former-commit-id: 10da3067b8a5b196c7c14d3ea110072eef969984

--- backend/src/main/java/com/ssafy/eureka/domain/user/dto/UserEntity.java
+++ backend/src/main/java/com/ssafy/eureka/domain/user/dto/UserEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bde197e7b1df9c2d50deebdef63fe4f401c6606bd499add5933bf7997968542a
-size 1820
+oid sha256:8afb59bc2c775b7a9ef0595cd10bfbc73a3ed1a8a74b5efde61f7e35e226dde1
+size 1835
