09ed00492bbac72c89f4159d09b0b56638fb7d92
이승집 <edward626kr@gmail.com>
2024-03-28 14:38:13 +0900

    Feat: QR스캔 페이지
    Change-Id: If381f77cc54fd26e1294ddfd55fe9c06cd222257
    
    Former-commit-id: 24289416aed03c8ee4637695f5e0b0f53a06bd28

--- frontend/src/routers/CardRouters.jsx
+++ frontend/src/routers/CardRouters.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40554a8fee21d0ace642237a6aed7ca40a2e9dcc9fd3cf1d740f5e444aa0854f
+oid sha256:8f8fce643fe5a88d3d28f54f33721c5fc4b79b9df1192634bef6765743dd2c7d
 size 978
