130fe2d0f9c72adebdde90146734ebd6ab12510f
김해인 <khi762@naver.com>
2024-03-28 13:43:51 +0900

    Chore: 라이브러리 추가
    react-native-vision-camera
    expo-image
    
    Change-Id: I9479340a9643f78a7c681e7b382e469efecfcb80
    
    Former-commit-id: fcb7dfeb98a2ea8250a4b4834d1dde05e8d6a5e5

--- frontend/src/pages/QRScanPage.jsx
+++ frontend/src/pages/QRScanPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4f5be8d6fe193b09198a5db14f393d42fc33d916f16c4b65f6a9bfc54943133
-size 12104
+oid sha256:fad99fd0b4e18e751ee75ce43ffd2ffb7818a94c180388766044862669aa61ab
+size 12108
