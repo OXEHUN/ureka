f5a427090fa16d2d6ea92a879d14fb70f7670f3b
yeongdae <oooo_0@naver.com>
2024-03-28 23:45:50 +0900

    Feat: 결제 요청 및 결제 승인 API 구현
    Change-Id: I2227a861e23e2ac97e65c84ba84f3feae9dd45b7
    
    Former-commit-id: 93bdcfde741a1359c32c00e017f6ec4d08e0a9bf

--- backend/src/main/java/com/ssafy/eureka/domain/pay/dto/PayInfo.java
+++ backend/src/main/java/com/ssafy/eureka/domain/pay/dto/PayInfo.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aaaf7d1bffbdcb96e8d38e837f1b07d466e963ecbd5290759b37dc0b8cc2b4aa
-size 1397
+oid sha256:3cb9007fe7c63554dc866a94d052c2e0a46ff426068ce00241352c8fcd747c21
+size 1965
