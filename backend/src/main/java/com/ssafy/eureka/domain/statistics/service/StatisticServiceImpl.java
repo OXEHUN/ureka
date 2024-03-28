37cd86f76c2d351859bca601c30e62b888dceb38
김해인 <khi762@naver.com>
2024-03-29 07:45:59 +0900

    Fix: 전체 통계 디버그 코드 추가
    Change-Id: Idbcc302513071053c01ce6e3b2ed2108a6eb2e54
    
    Former-commit-id: 29a11e25d3181bb0e4fa0867cd85eab640f476af

--- backend/src/main/java/com/ssafy/eureka/domain/statistics/service/StatisticServiceImpl.java
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/service/StatisticServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3d489797772cb27257c3e85de71b96cc14a291b1c5ed352a672246c9dcb4750
-size 2639
+oid sha256:9535db2043439cdcd371c51d78242190547d0a68aa8e102d5f070d56ed54bf7c
+size 3086
