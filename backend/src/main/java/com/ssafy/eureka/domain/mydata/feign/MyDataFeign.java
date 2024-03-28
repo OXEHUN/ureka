bda6894d181650a9d4b8b0d881aeae066e0042e2
yeongdae <oooo_0@naver.com>
2024-03-28 21:24:18 +0900

    Comment: 주석 삭제
    Change-Id: I9bf07e285b41e61479ca552308221daf25f99d92
    
    Former-commit-id: 8be15345a9c6b5c6c6569c0230f6beb6cf83cd0a

--- backend/src/main/java/com/ssafy/eureka/domain/mydata/feign/MyDataFeign.java
+++ backend/src/main/java/com/ssafy/eureka/domain/mydata/feign/MyDataFeign.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:310b8f9bdf3f327416c3bc67282b131bd86866e4fd4b79a4a7193885f0c8ed45
-size 1573
+oid sha256:80b72033e4c9bcfd39848e6f4234dbd6414fce64f895eb94b005a5dffe77ebfb
+size 1495
