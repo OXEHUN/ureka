b7f27a6109f105d23f6f2bd3f09e32f2bb9605db
yeongdae <oooo_0@naver.com>
2024-03-28 12:50:55 +0900

    Feat: 보유 카드 조회 카드사별 조회되도록 수정
    Change-Id: Iffc40acd8156b9883fd18681b38a67a71ed4647e
    
    Former-commit-id: 561b937010c65d0d3238946920dd4b5c6dd31b9c

--- card/src/main/java/com/ssafy/card/User/repository/UserCardRepository.java
+++ card/src/main/java/com/ssafy/card/User/repository/UserCardRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a29e64132104e1d5614c17d1c2853e608609a735e2db80598dc074a2c218475f
-size 856
+oid sha256:c7174d9c2a0edbe6f3ea821bfe42a8f5cbb1e843495c566413c034308699ad0d
+size 1169
