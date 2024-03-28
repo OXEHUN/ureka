34281e185d1e919b2d2573a5bf04f91b6c27fa91
kdw <hide4321@naver.com>
2024-03-28 14:05:01 +0900

    Fix: 결제 카드 없을 시 빈 값 반환
    Change-Id: I187a645c4d18939d8cff6a8d62260a3cde6c0440
    
    Former-commit-id: 3cba10811d2f6ac7094267ac3f80e8f71e413674

--- backend/src/main/java/com/ssafy/eureka/domain/card/repository/UserCardRepository.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/repository/UserCardRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:712cf20fa22c678bde87a2d146e9ed42b6bc116eb32dc5d7718cfe2f9120b987
-size 799
+oid sha256:e9b4e013895357897c60205a3ae0ee664fcb656a8e41e93d2f6d3a2a63af12d8
+size 855
