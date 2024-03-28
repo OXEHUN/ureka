c8c3d3f5d746f95e7014eccdb17c46fa18e0b812
yeongdae <oooo_0@naver.com>
2024-03-28 09:18:19 +0900

    Fix: Entity 수정으로 인한 데이터 타입 오류 수정
    Change-Id: I1b4b55f046c29923bdc4f83e6aa52c9ae023bd4b
    
    Former-commit-id: 17b40ce31e66ded07115b1a86c0dabb36ca04671

--- backend/src/main/java/com/ssafy/eureka/domain/card/dto/response/OwnUserCardResponse.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/dto/response/OwnUserCardResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4491d790e9bdb588f40bfc0fd5f4f05c055668d3988e3c4dbc7e5b9c9292097
-size 2040
+oid sha256:8c9cb56b364abb39cef833c2cf17da4f0d61dc5f10249c5c9b3539c5276f940d
+size 2075
--- backend/src/main/java/com/ssafy/eureka/domain/card/service/UserCardServiceImpl.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/service/UserCardServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f08e922163de59a5b08ca9805995437bc83449285c19c00ebfe45aa8714249d
-size 11995
+oid sha256:ca0b928e2fdf45d36cc1b73b9970ae97a487845c662a6df8701e6634848deccf
+size 11996
