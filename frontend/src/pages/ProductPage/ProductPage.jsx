a9d67a57da490b096de9a3b5e6e7704822975ac4
oksehun <sehun9803@naver.com>
2024-03-29 11:52:04 +0900

    Feat: ProductPage
    Change-Id: I9fed4e1a8efcd254d9ff4d0d023b75edee79805b
    
    Former-commit-id: 2b3b80577e4c86e84a9a1771b16a95d3963f2467

--- frontend/src/pages/ProductPage/ProductPage.jsx
+++ frontend/src/pages/ProductPage/ProductPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:488979f31dc40edb572a2da4e34b624c0cc2d298d384f843da776be81f9664cd
-size 6738
+oid sha256:5c7789714de1ec1797f76ddad8bf0baec1dba5ed3756121a83f3e5266e0341ca
+size 7582
