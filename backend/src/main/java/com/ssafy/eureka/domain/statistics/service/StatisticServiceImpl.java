1cfd9e95e3b1ef0c7381e158414c02878cc3f5ec
김해인 <khi762@naver.com>
2024-03-29 10:07:30 +0900

    Fix: 통계 보유카드가 없을 경우 404에러 반환
    Change-Id: I9c05e0570ecf399904a789dd4a4e83ca24a79a1f
    
    Former-commit-id: 9f0c063a7220b133a4f6f7daa58bcd0f2b2190f1

--- backend/src/main/java/com/ssafy/eureka/domain/statistics/service/StatisticServiceImpl.java
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/service/StatisticServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9535db2043439cdcd371c51d78242190547d0a68aa8e102d5f070d56ed54bf7c
-size 3086
+oid sha256:a5c118626b934b3dfce6615060fa604227663ff111d4d0714906f5de586fbec7
+size 3158
