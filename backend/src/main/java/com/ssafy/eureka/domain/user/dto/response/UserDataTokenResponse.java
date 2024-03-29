fc576938f0216a5a64310ea68dc068ddfd4e2598
yeongdae <oooo_0@naver.com>
2024-03-29 10:29:44 +0900

    Feat: 유저 로그인, 회원가입 리스폰스 변경
    Change-Id: I8fa30fe0d23b6e55028ac9adcbf68130cc1e3761
    
    Former-commit-id: f442057cc4222f5eed7fb83b362a8b7003eb4461

---
+++ backend/src/main/java/com/ssafy/eureka/domain/user/dto/response/UserDataTokenResponse.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:500157ec18c8920acba0f125e1da561ca4a9c7cdbec132ee61ec73c3f299627c
+size 570
--- backend/src/main/java/com/ssafy/eureka/domain/user/service/UserServiceImpl.java
+++ backend/src/main/java/com/ssafy/eureka/domain/user/service/UserServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15ad603534d2cd3d74181ec5d96f1027bbbb99108affb464b53e9dfb95753a95
-size 8839
+oid sha256:5d86ebb44963e00aa3d9149a6f04a8b555bfc3a87c4c354614eab5e535e9bea1
+size 9700
