27b7bcc7c185cbf9454a8e04375f88b5257f3e69
김해인 <khi762@naver.com>
2024-03-29 15:31:26 +0900

    Feat: 전체 소비 통계 조회
    Change-Id: I97cd5d62c6ed45526d317d04a6bf2b75062c5985
    
    Former-commit-id: bb60961e03cc83ec6af47eb7f1ca0abbf04c6a7f

--- backend/src/main/java/com/ssafy/eureka/domain/statistics/service/StatisticServiceImpl.java
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/service/StatisticServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5c118626b934b3dfce6615060fa604227663ff111d4d0714906f5de586fbec7
-size 3158
+oid sha256:f403918722120de236445d397e5ffaffd4c44127be398945d6b143b5bd3ce119
+size 6972
