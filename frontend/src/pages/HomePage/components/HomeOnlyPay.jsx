0e4811fc25911e56981409bd613ed073584b007b
이승집 <edward626kr@naver.com>
2024-03-30 15:33:00 +0900

    Feat: 우리 페이 결제 금액
    Change-Id: Id7e550afbb25038c21b035a237fe48bcf3a7f160
    
    Former-commit-id: 347f21b9a8123ceb76ffa1163190378644ccea8e

--- frontend/src/pages/HomePage/components/HomeOnlyPay.jsx
+++ frontend/src/pages/HomePage/components/HomeOnlyPay.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2705b6dab301a68dc198d919bbce7ba95eb41a9d252745d214425e40db4132bd
-size 1576
+oid sha256:3925ba6e2afa29f36ec53759c492d36773477951fdf40de1e63a7514a82284de
+size 2782
