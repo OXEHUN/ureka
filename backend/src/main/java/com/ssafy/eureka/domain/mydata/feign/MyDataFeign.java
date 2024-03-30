35c1cebbc498a213ea5f0b39c62cf246d329f3f4
yeongdae <oooo_0@naver.com>
2024-03-30 21:10:05 +0900

    Feat: 인증 문자 전송 시 존재 여부 확인 로직 추가
    Change-Id: I1f013f6f58c361a51c1d2b0df135b82454fc227c
    
    Former-commit-id: b25a4b5fb067b165fd0e6d73d06a8eaa15b6ed42

--- backend/src/main/java/com/ssafy/eureka/domain/mydata/feign/MyDataFeign.java
+++ backend/src/main/java/com/ssafy/eureka/domain/mydata/feign/MyDataFeign.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73355f0457233cffe4104486bcbc3144786eef698f148e27703af39f9275d261
-size 1471
+oid sha256:55f09e404a3aa2bc896e785d1f0555e278436d7e2d0275ade3c51bc0c3718e3a
+size 1615
--- card/src/main/java/com/ssafy/card/Auth/service/implementation/AuthServiceImpl.java
+++ card/src/main/java/com/ssafy/card/Auth/service/implementation/AuthServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:313545f3baec93e443268708e34c2ebc35cb550380f1d14662fc64dd518967b6
-size 4332
+oid sha256:5033956ddc9a9c0b8b990c2e920e0fa654586fd978326083ee2f42e4a01c7e92
+size 4593
