ff3278a329f243d99fddc9b41adf869fa11e8d0d
kdw <hide4321@naver.com>
2024-03-30 14:57:23 +0900

    Fix: 카드 등록 롤백
    Change-Id: I0b656a6f3e0aa17d6ca0a43483c24a0a98d41e04
    
    Former-commit-id: aa39210c1f3c25824203a32a613ace6b3258a384

--- backend/src/main/java/com/ssafy/eureka/domain/card/service/UserCardService.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/service/UserCardService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38f8f6ec58edede7fd57c3cb7e63426a8487727cd17fae4f885cb847d963694c
-size 1081
+oid sha256:5922501be2231871c3f5067a1bfb12ebd7f6aaac7ef7bd33c750f222e07ab9ad
+size 1066
