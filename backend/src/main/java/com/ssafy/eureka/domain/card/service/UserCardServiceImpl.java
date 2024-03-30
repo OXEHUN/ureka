d742fe83a9fc363042b7adf09a345e6ba6dfef67
kdw <hide4321@naver.com>
2024-03-30 14:36:54 +0900

    Fix: 보유 카드 통계 저장 에러
    Change-Id: I8835843c79aeec30a95d9a294325749d8b897823
    
    Former-commit-id: a42f791e524ac0e38cd307367aad5caf33b33f3a

--- backend/src/main/java/com/ssafy/eureka/domain/card/service/UserCardServiceImpl.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/service/UserCardServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea45ecb8b0786edfdd5508c6b56e553fe15f66f7cd935977b7e3bbe3b1e1cf54
-size 21527
+oid sha256:2da157624d227be4e9e3771fbdc4431254e002aef935383341230f65fa4b0c11
+size 21709
