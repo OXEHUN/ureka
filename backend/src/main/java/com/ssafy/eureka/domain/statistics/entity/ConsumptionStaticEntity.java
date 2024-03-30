e7755516c182f3ce2d88add515c99dceea4fc597
yeongdae <oooo_0@naver.com>
2024-03-30 20:36:12 +0900

    Fix: Merge Conflict
    Change-Id: I366d068e85586a30b13e00bf35d3d76a1d7b7c3f
    
    Former-commit-id: 00e9f148bbb44f959b404f4e8476cec489251cbe

--- backend/src/main/java/com/ssafy/eureka/domain/statistics/entity/ConsumptionStaticEntity.java
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/entity/ConsumptionStaticEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:70a4ae20aba8c67efcda916deac1226bb3eabadc217f0bc441fd3060bc4f2519
-size 673
+oid sha256:72c559f22e3e00910635ef7ccc43dc2692b64a34306713b7b01ac73e617c63c1
+size 707
