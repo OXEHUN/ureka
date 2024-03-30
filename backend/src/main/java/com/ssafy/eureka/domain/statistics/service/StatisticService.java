6595339e992ab5eb005039c6b92adf62e0389131
김해인 <khi762@naver.com>
2024-03-30 23:53:55 +0900

    Feat: 이달의 할인 카드
    Change-Id: I36b00a724971320e41d3c04cb78d9add8d2ef7d7
    
    Former-commit-id: 1e4514d43929e9b26ecd188402f0117b00ed7e0b

--- backend/src/main/java/com/ssafy/eureka/domain/statistics/service/StatisticService.java
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/service/StatisticService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de331cfa68b4363a5f73390260ede774dec4489ddfa03557010841ff023981aa
-size 783
+oid sha256:34e8bdf5dbe47e4cb3a8fa2e24ed344df8d7a67982381049ba44c23f3d18e195
+size 955
