54d6b778de93d2b9c894c0f90a0c90bcd993c52a
옥세훈 <sehun9803@gmail.com>
2024-03-31 23:48:08 +0900

    Feat: make benefitgraph
    Change-Id: Ib9f78cf98e5a5d1ce9f7f401b14657e3fa565471
    
    Former-commit-id: ed92e65d13b3797d6cc77e5f73f57341892922f4

--- frontend/src/apis/StatisticsApi.jsx
+++ frontend/src/apis/StatisticsApi.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10198a6772273c8c2611d6512519a2a5f03ab35eca68548d9192d6a67112529c
-size 537
+oid sha256:b36daa4fc5c7f87be4bcf204b9bc47c95b03c8b4722ae84b54292098c01a2e3f
+size 1022
