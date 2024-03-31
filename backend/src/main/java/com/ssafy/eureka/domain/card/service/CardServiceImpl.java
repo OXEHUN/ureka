01ecec5ff749535e1c68e9a4fe29617f2707e6eb
kdw <hide4321@naver.com>
2024-03-31 15:33:20 +0900

    Feat: 카드 상품 상세 정보 조회 JOIN PATH 추가
    Change-Id: I6555e3a5a03c5f40670a37ee233c6fadc6e68ebf
    
    Former-commit-id: e57a8a1fe2f5a29e7d409476d6bae5f41559c2f0

--- backend/src/main/java/com/ssafy/eureka/domain/card/service/CardServiceImpl.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/service/CardServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f10521aeb1ab515ca10ffbc144d7743f84b54ea5eba31e2adcb1add84e791419
-size 9265
+oid sha256:f81639a1ab5706c82badb6e04c7fbcc68473dd6aaf3b081e723c0e1e43440ce4
+size 9327
