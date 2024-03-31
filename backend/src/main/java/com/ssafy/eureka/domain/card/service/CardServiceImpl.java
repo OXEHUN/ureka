bd822a6870999cdcfd9c6feae3a8b05b01c0873c
kdw <hide4321@naver.com>
2024-03-31 15:05:26 +0900

    Fix: 주석 제거
    Change-Id: Id50589b5c22ee35122a78569c12c4c1b9c00d767
    
    Former-commit-id: d36d3d6e4b27c558a8b0766f3bac0ac8f8931163

--- backend/src/main/java/com/ssafy/eureka/domain/card/service/CardServiceImpl.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/service/CardServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45927ae675b067d9689251ac8ac10a7eb04122a432a9886ce48e8bddbd746821
-size 10634
+oid sha256:f10521aeb1ab515ca10ffbc144d7743f84b54ea5eba31e2adcb1add84e791419
+size 9265
