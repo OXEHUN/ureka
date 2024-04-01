ddde5f307d531c4ea6e45bfca769c32633eec6d1
이승집 <edward626kr@gmail.com>
2024-04-01 15:40:56 +0900

    Feat: 카드 등록시 모달창& 결제 완료
    Change-Id: If48c159154796e91808bd10c03964ac1b4b03b84
    
    Former-commit-id: ec8e97d5cdd6ef9f78adaf070f8963f2833ae217

--- frontend/src/pages/QRScanPage/PayLoadingPage.jsx
+++ frontend/src/pages/QRScanPage/PayLoadingPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da94b4b60e71687ad677ab2d4a6db3b100df428b01826ed044e289a7a5f95a96
+oid sha256:739bf8f9a335b5c291eb6a751683d3d1ad002f05e2c79918e3f6a531879092cd
 size 1484
