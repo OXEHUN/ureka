88471dbfcd27c3de193c15d2ce38666951011e66
yeongdae <oooo_0@naver.com>
2024-04-01 11:09:45 +0900

    Feat: 결제 요청 할인 타입 응답 수정
    Change-Id: Ifd07ff3c5056ecd1e81355345ef5196024687a7b
    
    Former-commit-id: 94a0994640c2948faa5854adacffa3586495e706

--- backend/src/main/java/com/ssafy/eureka/domain/pay/service/PayServiceImpl.java
+++ backend/src/main/java/com/ssafy/eureka/domain/pay/service/PayServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46ca00c09ac041f2482dfd3de975a01551b67348c8d7cae18ac8df76df154a65
-size 18771
+oid sha256:0fd8ede972931f6ead866d539dd7db1904d60c01fca90af8196f66675ef31e55
+size 18892
