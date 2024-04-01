1b4d2b1ca6ce465fd93eb0f26678b591280a2494
oksehun <sehun9803@naver.com>
2024-04-01 10:15:38 +0900

    Feat: some stati
    Change-Id: I4d97ff1ad31361007206ccbf7e5f098b81281c9c
    
    Former-commit-id: 77cc38fe9ce4ee548a78c9c7332df6eb715b1265

--- frontend/src/pages/StatisticsPage/components/CardRankingComponents/CardRakingList.jsx
+++ frontend/src/pages/StatisticsPage/components/CardRankingComponents/CardRakingList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0faee27c4b796364105c93abe7cca0bcb13fb541c0a2585e3288043d9f901357
-size 4507
+oid sha256:73e45ebfb2feea33e57810f21c3f78fe0c3391cdd887783d79c6f308819c8207
+size 4704
--- frontend/src/pages/StatisticsPage/components/ConsumptionOfYou.jsx
+++ frontend/src/pages/StatisticsPage/components/ConsumptionOfYou.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5333e3a1ac3ff8f1fc798e72ad45dcb2db1b0254dacb8f4b31b6d8b5fd5ac46b
-size 5670
+oid sha256:ca96c8650bb4de8d94ebd90cc73ad13ab88008e29f6f7b12ceed62507e3af9de
+size 5777
