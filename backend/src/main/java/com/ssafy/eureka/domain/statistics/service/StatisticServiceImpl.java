87127bb6e4fe4ca3630d254409818960cc4aa651
김해인 <khi762@naver.com>
2024-04-01 21:35:46 +0900

    Fix: 가장 최근 데이터만 조회하는 로직 추가
    Change-Id: I78569b21d0171e78d0c78870956393c25d3f3d98
    
    Former-commit-id: 9282b59d3f8f23cbc68d99db288b816e5b78dc55

--- backend/src/main/java/com/ssafy/eureka/domain/statistics/service/StatisticServiceImpl.java
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/service/StatisticServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b6541efedad903f01eadf2dfc5928ebd8f7834e8c31c99856068fb45726ad63
-size 12358
+oid sha256:2e4e6d9b6cbbc75c6cdea2bad27d462d0c9fd26410dcdd1e632f2feaf24a2a78
+size 12560
