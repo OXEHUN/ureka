50405e3017b83a24aa0893f000e50482830ead5e
김해인 <khi762@naver.com>
2024-04-01 11:58:14 +0900

    Fix: 생성자 오류 수정
    Change-Id: I6b8fea39fc98f4eb7140619a76a96f70ef8f00eb
    
    Former-commit-id: f0fdb1419bdc18d837c751630a83a51c5e356362

--- backend/src/main/java/com/ssafy/eureka/domain/statistics/entity/ConsumptionSmallStaticEntity.java
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/entity/ConsumptionSmallStaticEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14bb57e9b1e3320b882330c315d1698b1b4488172c845008e129be334fbf76b3
-size 1515
+oid sha256:0ad749f13e9126b3823e36c55727d5ba60d4d0ed6701b096d96088c5bedcbcb5
+size 1570
--- backend/src/main/java/com/ssafy/eureka/domain/statistics/entity/DiscountLargeStaticEntity.java
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/entity/DiscountLargeStaticEntity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dfc11f2dfefd44024a09488c499f8e3b93cfb6ede40cf10eb0b921eef9cc33d0
-size 973
+oid sha256:3c4eac4f613faa87841122cc1ef6163a5616c614d363cbdf226d72a8ff0d6e64
+size 1080
