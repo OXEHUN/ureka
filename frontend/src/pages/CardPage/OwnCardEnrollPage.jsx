cc5dfdb6dcd94ba6840f1dd507bcf4c4bf1ddeb5
이승집 <edward626kr@gmail.com>
2024-04-01 10:48:15 +0900

    Fix: 보유카드 등록 한번에 되도록
    Change-Id: I93d7289c87a19ef997832fd2d56fc53a8d73f1a2
    
    Former-commit-id: 13d3f1b2e1ba8ccc3df21e08ce0410ace1e38220

--- frontend/src/pages/CardPage/OwnCardEnrollPage.jsx
+++ frontend/src/pages/CardPage/OwnCardEnrollPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bbe56f7a3afe18f938bef233251b3c3b8afff50b461e7a878dcd066dfe23912f
-size 7361
+oid sha256:93ed0cecb149922743fea0c75956571869b7809daa11035435c3b6becc4d29cb
+size 7377
