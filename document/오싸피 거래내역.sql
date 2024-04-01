0898cd16a27601d604db71a08b1b14fecb34c3e1
김해인 <khi762@naver.com>
2024-04-01 16:44:17 +0900

    Docs: 더미 추가
    Change-Id: Iecf05e85abc84468a69cd33b38fdb956d614e176
    
    Former-commit-id: 9d5f8477f73403cafb6a548d6c33298b2070fd5f

--- document/오싸피 거래내역.sql
+++ document/오싸피 거래내역.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:67e5b9b542b4bf137144fe98c25846e34bfdceaf94bb92c9685238a62930e1a6
-size 94324
+oid sha256:acfd632142e0da606bda2407b18c8c9991a9e7e6f0a12916fd78970cb2ccabb2
+size 93987
