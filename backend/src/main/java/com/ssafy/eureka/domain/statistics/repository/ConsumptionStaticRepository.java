c20ca94df03fb098e4126f332d6c979d1c9598ab
yeongdae <oooo_0@naver.com>
2024-04-01 12:22:23 +0900

    Feat: 결제 요청 시 이번 달 소비 금액 오류 수정
    Change-Id: Iefa3ac156d3f3cc985fc8dadaa19933f7c78f3cc
    
    Former-commit-id: ff999430afa96c0e52809bfdfaf2991938004540

--- backend/src/main/java/com/ssafy/eureka/domain/statistics/repository/ConsumptionStaticRepository.java
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/repository/ConsumptionStaticRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5cf640ae65b40c4d555c714b5aecf68a826e7bdd5c7c21d2775186f485afdc50
-size 1413
+oid sha256:f685f86c66f123681f5721cffdd9d74d26c4f7a5e7f68a75376bb48bd9574ebb
+size 1333
