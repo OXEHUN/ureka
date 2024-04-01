314dd41c06f8232c11a459d192db34719c8bae35
yeongdae <oooo_0@naver.com>
2024-04-01 12:06:40 +0900

    Feat: 결제 요청 로직 수정
    Change-Id: Ifbfa764c6ecbfc1738fb523c673458ba8848f275
    
    Former-commit-id: dae17d4b12eb9b8406bd199effba306de50284bb

--- document/eureka_db.sql
+++ document/eureka_db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3201c2a0df6e8aa68384c5dc7fc6dcf1bdddc55ac44643fc2ef7ba59f1f26179
+oid sha256:d12cabc8a706f87921dc9c65fb04860bfdbfa22eb02a932b8d6a09e081fbffa7
 size 15836
--- document/오싸피 거래내역.sql
+++ document/오싸피 거래내역.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44ed772b567d5920039ee653950b66f734e83c9a27b311039b07daac248f8326
-size 93786
+oid sha256:6e1f3c79935c0b72f0bc9d87c4c58bb4ae3fd1833d2d15264b72dcd818756fee
+size 94594
