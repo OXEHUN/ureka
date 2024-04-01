90e9934916809e7711dd40bb3f825165fba03001
김해인 <khi762@naver.com>
2024-04-02 07:46:12 +0900

    Feat: 지난달 소비 비교
    Change-Id: If7454c74aa64987ef090cb14bec837bcfcab6889
    
    Former-commit-id: 4c464dcb76f12af8a0c1536b8f2eaf64746bbee2

--- backend/src/main/java/com/ssafy/eureka/domain/statistics/repository/ConsumptionLargeStaticRepository.java
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/repository/ConsumptionLargeStaticRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc6f4c78aac1db2b1a1455d13b32acadc6fabb9c4fb3d42a5af9b35c779b8263
-size 3015
+oid sha256:9cb5af2c179157f5fc9a701b7d459cdd98071da709f3f9fe0066ff2477fcc994
+size 4153
