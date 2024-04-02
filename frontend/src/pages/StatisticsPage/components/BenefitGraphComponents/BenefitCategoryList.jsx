1415a4e33033b1372fb2d1ff4155a7753a3d3fe3
옥세훈 <sehun9803@gmail.com>
2024-04-03 05:03:52 +0900

    Fix: static err ㅠㅠ
    Change-Id: Iec35c07d1a6da07ebb084d7474437f44a6d78243
    
    Former-commit-id: 35edc025dac194ad9f5ea30ca74612a9aadb0183

--- frontend/src/pages/StatisticsPage/components/BenefitGraphComponents/BenefitCategoryList.jsx
+++ frontend/src/pages/StatisticsPage/components/BenefitGraphComponents/BenefitCategoryList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf6ece7a22cc6eba7a2a1e4900cd2e79b95a346fc7c73eef5728bbd6c6f0fb38
-size 4034
+oid sha256:657a7f9d5751c80891c9da61ead5421d4b54127810c2ee1fa807fbb10754b788
+size 4067
