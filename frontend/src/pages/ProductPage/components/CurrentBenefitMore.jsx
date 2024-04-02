e657f521e1aa5218434801f438cb0b0352a7f150
oksehun <sehun9803@naver.com>
2024-04-02 10:00:48 +0900

    Design: CardImg
    Change-Id: I45c4d75e5e849c066be37df7dd5dcd99d01fe9d6
    
    Former-commit-id: b9b07101566c5063e7de88acda2fa574d16c5d19

--- frontend/src/pages/ProductPage/components/CurrentBenefitMore.jsx
+++ frontend/src/pages/ProductPage/components/CurrentBenefitMore.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:805a81a00c2fd29b415a552cde7c7db3a3927e0fcdef0a66f7d679a3011a5235
-size 5722
+oid sha256:5164d4ca557c37aed74bf38c14d8f1dbfd450ee8959aedd9135d59036cd1e759
+size 5765
