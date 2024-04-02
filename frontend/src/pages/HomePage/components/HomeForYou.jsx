f52251f3167dd4b33bddfde4e0abf81db9cdc28c
최용훈 <cyh2607@gmail.com>
2024-04-03 01:22:59 +0900

    FIX:homeforyou
    Change-Id: I998b6f3b5bcb2c18eada9957899230199b8e61c6
    
    Former-commit-id: 312e1ff5e184b0ce6c84523955c3b9f45168a09c

--- frontend/src/pages/HomePage/components/HomeForYou.jsx
+++ frontend/src/pages/HomePage/components/HomeForYou.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03058df95efe67001705b95e0c194637bc8fbedeb59092acdb88e2ee7e179376
-size 2925
+oid sha256:d5612e6cca99247709e47432a0f4fcfb7c30818088d6ac3f4d7d6c0d9aefe1ba
+size 2898
