2d9ffe7287a43fe3622abb6f93ed9c91c6b887f4
oksehun <sehun9803@naver.com>
2024-04-02 11:39:22 +0900

    Fix: product-page
    Change-Id: I6b84a737912676bad088b6544765eceb556c16fb
    
    Former-commit-id: eedcd6a95a444325c0d44e31be4b9eb15aab3b31

--- frontend/src/pages/ProductPage/components/CurrentBenefit.jsx
+++ frontend/src/pages/ProductPage/components/CurrentBenefit.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ce7f4ac69ceffc768751cdef8baa2b798ac4c712b9e1746c2cdebf1464bd2e9
-size 2793
+oid sha256:0be1daf1a0e7b632726e707d3f7ab13e4ec967634af8706653db4122899aef9f
+size 2872
--- frontend/src/pages/ProductPage/components/IfUseRecommendCard.jsx
+++ frontend/src/pages/ProductPage/components/IfUseRecommendCard.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d621f71361ee0202cb273d14eb57be115a062532dddb00da031d121c8a70f2c
-size 6763
+oid sha256:478a604069b8ea0689f3e4d906851e582b66afe00fa68651fda8402e31d1e93d
+size 6765
