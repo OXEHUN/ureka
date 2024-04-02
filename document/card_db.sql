c6fd3010916deb834835d6e63e291b3b2dee910e
yeongdae <oooo_0@naver.com>
2024-04-02 23:29:53 +0900

    Feat: 더미데이터 수정 / web 상점 목록 추가
    Change-Id: If3763ea61fd16f3990c7c18d58f905709c37ffff
    
    Former-commit-id: 1cc4ad3c1fc39003a288b96c68ee20288926a09f

--- document/card_db.sql
+++ document/card_db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41ff2b6d3615d90b02c02b2dba8ed1f2bee38a38e131a866cca890fe4d5352ac
-size 5591
+oid sha256:395d9425749a1a556d0750f43ec8a3691beb333d4376198890589044ad160926
+size 5590
--- document/삼싸피 거래내역.sql
+++ document/삼싸피 거래내역.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3971b4705dfd0dae3f82876912102f19e75974fa23b05f1af1eaddfbb79c5c41
-size 10541
+oid sha256:f0d9138a138d338ae2567e89fc978f6e24276f18c406b622522a6a521412f368
+size 31252
