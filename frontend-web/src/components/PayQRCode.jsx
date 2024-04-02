236fb2c13a7dc0ec5d9989bbddc0aaaab78760a6
yeongdae <oooo_0@naver.com>
2024-04-02 23:30:53 +0900

    Fix: web 오타 수정
    Change-Id: I682bc7175f295b231f40fecd8936bc9ddf8c33bf
    
    Former-commit-id: d6f2a5783f0afe03f5ddb15fcea12e643f71240c

--- frontend-web/src/components/PayQRCode.jsx
+++ frontend-web/src/components/PayQRCode.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffbb6ed4c880e9b5929b06ba92228dff8f4fc66d9e44731f884615245463d8e6
-size 3992
+oid sha256:a509a655e11a72da971368fb35b9b48a961968530e2806d629b47699e19bcc7b
+size 3991
