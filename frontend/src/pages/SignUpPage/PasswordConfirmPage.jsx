bd7b0b63a1879d358d06039e67411e3eb4aea36c
최용훈 <cyh2607@gmail.com>
2024-04-02 23:34:07 +0900

    Fix:생체 인식 인증 물어보기 추가
    Change-Id: If287215c396fb7ac0f95ace6886d59ebdc71ce81
    
    Former-commit-id: 630ea010d050d4afd59d519fbb11b08d26d7da7f

--- frontend/src/pages/SignUpPage/PasswordConfirmPage.jsx
+++ frontend/src/pages/SignUpPage/PasswordConfirmPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1752271cf5c1f4578faae3b50b1782f86f083440e70a36bb765f010716c0842
-size 9094
+oid sha256:c79f8b4019f6d3b15dc8226da953c52da7fa4917207a300aca61c54e6e9ba2ab
+size 10034
