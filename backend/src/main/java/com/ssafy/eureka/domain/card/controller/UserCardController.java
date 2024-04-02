a7b4358d251b7639925a754c504a2090e96630e9
yeongdae <oooo_0@naver.com>
2024-04-03 02:09:00 +0900

    Feat: 카드 상품 첫 화면 API 구현 / 또래 카드 추천 응답 DTO 변경
    Change-Id: I0383be3d33225a688a7fd51f052dde2eac108ac4
    
    Former-commit-id: 2685546d83ac65d4931d02a42e8fb7c23a68cd6a

--- backend/src/main/java/com/ssafy/eureka/domain/card/controller/UserCardController.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/controller/UserCardController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee304537e356ab021a7cb3c95c0b45cf6e9b29721ab32191f19f934195037ffb
-size 6433
+oid sha256:7a802cd8debac592865655b68be3052a17e7f6817f5da6a9994117f831d778ef
+size 6872
---
+++ backend/src/main/java/com/ssafy/eureka/domain/card/dto/response/CardRecommendMainResponse.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d6e41bfef6f4381900f3a23027d314aff9c63cd58c1cec2e382167fe405eae74
+size 1033
