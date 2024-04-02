4642db616072d6b52a9d5b1915b18669dc908979
kdw <hide4321@naver.com>
2024-04-02 11:34:45 +0900

    Fix: 결제 카드 조회 카드사 이름 추가
    Change-Id: I6344e988973f784e9efa865178f43c689b1f719d
    
    Former-commit-id: f8295b786d5f040db3d884e5713781f7958d31c5

--- backend/src/main/java/com/ssafy/eureka/domain/card/dto/response/PayUserCardResponse.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/dto/response/PayUserCardResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91281db9ea469c500d6f7a3fb50e2c817636f1768cb46396f7c80c76fe6153fe
+oid sha256:ed7b7943c537852d365b323045f18b1e09fed97aaf1662808e56cf5b794920f9
 size 554
