2a616f97b0a9ad39118db6399625dde3b4ed9f5d
최용훈 <cyh2607@gmail.com>
2024-04-02 17:28:32 +0900

    Fix:지문인증 및 간편비밀번호
    Change-Id: Ie1c05b2a093fe23d7af0e513fbfb4cd8f2c0a32e
    
    Former-commit-id: 8b9271323d8920dc5c39e899566de618bc676eeb

--- frontend/src/pages/SettingPage/components/PaymentPassword.jsx
+++ frontend/src/pages/SettingPage/components/PaymentPassword.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8634a1fc22d80288482fe98e1df77c32c3c6b09de4b8691eda45094392b92e57
-size 7375
+oid sha256:7a6951eb7a926b52676326ed889a26c9fd51be64b5d80849d44907e58c7e4e74
+size 7431
