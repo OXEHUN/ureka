457857b78bdd3958d88077a36148e6195581d819
yeongdae <oooo_0@naver.com>
2024-04-02 12:39:14 +0900

    Fix: 카테괴별 상위 카드와 비교 / 중복 카드 오류 수정
    Change-Id: Ic85cf7fb96250216c632c25e15aa58230dcf92ff
    
    Former-commit-id: 01590c9736df65093cc2f76a4ec29d0addf38139

--- backend/src/main/java/com/ssafy/eureka/domain/statistics/repository/ConsumptionLargeStaticRepository.java
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/repository/ConsumptionLargeStaticRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc6f4c78aac1db2b1a1455d13b32acadc6fabb9c4fb3d42a5af9b35c779b8263
-size 3015
+oid sha256:a2532d1c548c7e81b7ebebf77a4d224112512c63cefe9281428257d62044cd43
+size 3016
