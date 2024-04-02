b9590abb1733b99c94468c1c5bd7fc5364e66da6
최용훈 <cyh2607@gmail.com>
2024-04-03 01:17:29 +0900

    FIX:homeforyou import 한 줄 추가
    Change-Id: Id39389bab5143e65ca0a37fa7df5d57ed4f34472
    
    Former-commit-id: 3c35823ae618fd3b023ea10f6a9c33f423249ee8

--- frontend/src/pages/HomePage/components/HomeForYou.jsx
+++ frontend/src/pages/HomePage/components/HomeForYou.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31f823e9b6d502e95bfb514557c84d2538b02d89013852b4ed4dd610e0efef4f
-size 2871
+oid sha256:03058df95efe67001705b95e0c194637bc8fbedeb59092acdb88e2ee7e179376
+size 2925
