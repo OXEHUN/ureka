4990f4330880c254d0bd5cc9d005c33bc73264de
yeongdae <oooo_0@naver.com>
2024-04-02 15:09:27 +0900

    Feat: 또래추천카드 리스폰스 수정 및 결제로직에서 이번 달 소비내역으로 오류 수정
    Change-Id: Ieaf2a6936d091c34a2d176109f799c74ddc96605
    
    Former-commit-id: ba0d9d1db757f745d9de1a5ea916f43254b59cfd

--- backend/src/main/java/com/ssafy/eureka/domain/statistics/controller/StatisticsController.java
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/controller/StatisticsController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a676ed847ca849898cb95a3353082f7328a03da1ad9e7bd08cdd278b20d4bfb3
-size 5324
+oid sha256:eff56a5e29ec10b8cd7095cf977ae1089f745445e8aa05c8652df1678c17559a
+size 5370
--- backend/src/main/java/com/ssafy/eureka/domain/statistics/service/StatisticService.java
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/service/StatisticService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d662966e7721699b5329d451a72bc7c4d4ae59ffba2a423bfce325427617fbf
-size 1240
+oid sha256:bf2d9e0481d5e4b79e2b7d81ad27527cf84bb4ae3e52abc7a575a1aa4f9f0004
+size 1256
