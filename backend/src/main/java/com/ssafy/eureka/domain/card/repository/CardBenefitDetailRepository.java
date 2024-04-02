7675b97b37c2aac1be96e7ae00de8bd8e4ff7f73
yeongdae <oooo_0@naver.com>
2024-04-02 23:40:11 +0900

    Fix: 레퍼지토리 오류 수정
    Change-Id: I64bca1e4cc896068181119a513edbe844a5e0983
    
    Former-commit-id: 8bc90f6c2b2d53c3835cf5b945e27b8ca0ab8a3e

--- backend/src/main/java/com/ssafy/eureka/domain/card/repository/CardBenefitDetailRepository.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/repository/CardBenefitDetailRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d7ea82be4a4e564df71f6a1b5cec19939958e116535d9eee0c8e26189f22289
-size 4380
+oid sha256:79507e40e622a66323b1f00f4763853c56a1d87e9c4d1ce145de08e76c5f77b6
+size 3836
