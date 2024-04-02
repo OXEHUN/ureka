b39eed4150cd56a8f65778bc1e08c407b14fcfd1
kdw <hide4321@naver.com>
2024-04-02 14:41:15 +0900

    Fix: 상위 카테고리 3개 카드 추천
    Change-Id: I2ce24584f12a500b0554e5a2473b041b054d7d9a
    
    Former-commit-id: 7dd5583e8ce0b023a178762106365134617d988d

--- backend/src/main/java/com/ssafy/eureka/domain/card/service/UserCardServiceImpl.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/service/UserCardServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7145c34ab549345fd04fa45ac9498f2985ceb2d0192f61de2f4bea228810b806
-size 33272
+oid sha256:9ba5cd8cda021b393c59f08fb46129ac64584250f162f5b3c9d608b5910b1854
+size 33925
