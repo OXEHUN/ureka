a154d95b37b809be596a6f69dff6b5c774c56c83
oksehun <sehun9803@naver.com>
2024-04-02 17:57:14 +0900

    Fix: card Image and static err
    Change-Id: I1edf4a25a2264088c4e127d8f45e6e215432c027
    
    Former-commit-id: 116d74184a91f25b60c156154f3db5daa445f444

--- frontend/src/pages/StatisticsPage/components/ConsumptionOfYouComponents/ConsumptionCategoryList.jsx
+++ frontend/src/pages/StatisticsPage/components/ConsumptionOfYouComponents/ConsumptionCategoryList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9b3ca0bd6aafd3966ba8302aea1b6e4e3d12de8d431790ad40a214dba4dc2df
-size 3994
+oid sha256:583ff8e5930d5b3fb6e33645df68f31e1bb7bc0613f1a7e73b1ae8e642f83a84
+size 4044
