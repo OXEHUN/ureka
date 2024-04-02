020a754ca494d21f4b3c872763ec956366865107
최용훈 <cyh2607@gmail.com>
2024-04-02 17:28:32 +0900

    Fix:지문인증 및 간편비밀번호
    Change-Id: Ie1c05b2a093fe23d7af0e513fbfb4cd8f2c0a32e
    
    Former-commit-id: d7da731a28bd06feeb6cdfbeef27ab2bd6f448b3

--- frontend/src/pages/CardPage/OwnCardPage2.jsx
+++ frontend/src/pages/CardPage/OwnCardPage2.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88ccfd9e3e9db0f9534e25f8c9fa64d8202faafd18df27afbfdafda1fc826b2e
-size 4456
+oid sha256:e04d41950781f90d8aa9f46de6948082f4f399e3161c388673f249f5134ef9e7
+size 4384
--- frontend/src/pages/QRScanPage/PayCheck.jsx
+++ frontend/src/pages/QRScanPage/PayCheck.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce5b2254996cf104133469d8922e869941e5c42863e70a8d6afb6f797e8441b2
-size 9842
+oid sha256:dca1e6d093942e6134eebbe12578a97cf4452713f68691f8e001f91a7323edec
+size 10274
