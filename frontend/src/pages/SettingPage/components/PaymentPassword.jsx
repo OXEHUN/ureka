2b4128d8d36617dfcf939f5ffce583c2fc71ce45
최용훈 <cyh2607@gmail.com>
2024-04-03 22:41:21 +0900

    FIX:QR결제
    Change-Id: Ie0365fb4369ca5f4bb842fe70a8b7d9a520497ce
    
    Former-commit-id: 419cbe2a2eef287e1da47663b6aab79dc389e870

--- frontend/src/pages/SettingPage/components/PaymentPassword.jsx
+++ frontend/src/pages/SettingPage/components/PaymentPassword.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d856e04da3ac27c6c67a865ad1e5c4c0c04113aa58624cf17174cd29f31db37
-size 8447
+oid sha256:cf34ebe86ae11d742edeb91c5c151a8ce41e56877c56a227358b19c31fdaac76
+size 8474
