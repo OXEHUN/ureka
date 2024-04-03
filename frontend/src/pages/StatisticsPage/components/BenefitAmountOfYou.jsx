8023a0a8641de602be98dd8c8187ad5075d5bc0f
oksehun <sehun9803@naver.com>
2024-04-04 05:57:41 +0900

    Fix: 달넘기기, 카드랭킹, css 수정
    Change-Id: Iec321645c33e695de2f8789c63947e575674064b
    
    Former-commit-id: a2dcde1f5726e4257ee02d7b2c27343e67afe3c4

--- frontend/src/pages/StatisticsPage/components/BenefitAmountOfYou.jsx
+++ frontend/src/pages/StatisticsPage/components/BenefitAmountOfYou.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:677bcbe0ce00b67fa81e70c069577be1fa87a9585f1ee9663b1b819295954033
-size 2497
+oid sha256:ce033fbba705298d6470473a43eeaa3edb54f311ebb463a01c009e33733e3537
+size 2507
--- frontend/src/pages/StatisticsPage/components/CardRankingComponents/CardRakingList.jsx
+++ frontend/src/pages/StatisticsPage/components/CardRankingComponents/CardRakingList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e62840952e3b69b69e10140fc4fa8255ad7cc473cc8ec07cf71fa410b3782f8d
-size 5743
+oid sha256:beddff181d3dcb5d83454090412f38d68b77b8940632492e05fb634479b0ef72
+size 5780
