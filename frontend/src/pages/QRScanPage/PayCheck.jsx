503e8a41f8c1719edcf1a7e08526da0afe5ea18a
김해인 <khi762@naver.com>
2024-04-04 03:23:38 +0900

    Fix: 결제화면 수정
    Change-Id: Ia6acd253dc83dca56ac2e564a213a42ee5e7ec10
    
    Former-commit-id: dc94947100d509066318a1212a8fb1e51a6df23c

--- frontend/src/pages/QRScanPage/PayCheck.jsx
+++ frontend/src/pages/QRScanPage/PayCheck.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1bc6bfeabf94a04976460a4a8232c2906f37816041badd6db406018353ffa1b
-size 10590
+oid sha256:b66dff7c3854d5bcad3b6ca12ea58fb30ba26b3765b4ab0092286ea8ae530893
+size 11601
