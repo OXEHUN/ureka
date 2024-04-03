901e242f2828fd3a72f38809cb1bcff5cf1fc4ef
김해인 <khi762@naver.com>
2024-04-04 03:25:30 +0900

    Fix: 결제화면 수정
    Change-Id: I20253641c72cfdef3ad7739905bd24abac6c927e
    
    Former-commit-id: 363b38201d43e17eea6eb40f7484ce854ee9738a

--- frontend/src/pages/QRScanPage/PayComplete.jsx
+++ frontend/src/pages/QRScanPage/PayComplete.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5a01131d293d63bb2a646174bbd8cc17b93ab3ceecab8a3d07343f42fb7184c
-size 4097
+oid sha256:35204e0f808b1f4b7fe7177b9515770de7b9615c5525e6d6ac6b81b791a1dcae
+size 4095
