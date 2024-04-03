5cd7dbbbbf7a0d0c960a1bcb502f8a3ade53883e
최용훈 <cyh2607@gmail.com>
2024-04-04 02:28:50 +0900

    FIX: 결제내역 간격 수정
    Change-Id: Icfe8bc219f7b62f54e364809292ebabaa2df9e32
    
    Former-commit-id: f4618c0725439d9611ed059843d4b4668d887c03

--- frontend/src/pages/HomePage/OnlyPay.jsx
+++ frontend/src/pages/HomePage/OnlyPay.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2060f6c8f658c4ed474f54b829c7ab3b350381216b89ca01c1e6fff25224a49
-size 8938
+oid sha256:33cf242a9385a8b7861a0d2dd46fc18dbeff3fd6e63d7f27771073c4e32ec396
+size 9073
