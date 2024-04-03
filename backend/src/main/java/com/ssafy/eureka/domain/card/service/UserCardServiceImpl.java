e966283025a8fcc522fc1f4a45e8f590dcd99e6d
yeongdae <oooo_0@naver.com>
2024-04-03 10:21:53 +0900

    Feat: 더미데이터 추가 및 카드 상품 API 더미 데이터로 임시 구현
    Change-Id: Ie1e2f48f438df2a1fe16afb0f08b56f60971f2e2
    
    Former-commit-id: 56e8216f8122ba26a5fdacd862febff67e772cda

--- backend/src/main/java/com/ssafy/eureka/domain/card/service/UserCardServiceImpl.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/service/UserCardServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:717d53b55d6c1cd75f6f03dd98c22fece2c74cb9b86004c6d286353000642570
-size 36518
+oid sha256:bef8932f7d577b8d89b7ebd6a7a8fe8c7d7b8a9adfd6209f68b0836921bdd12e
+size 37086
