b68f472f7349bddc2e32965954c8db51de54b523
이승집 <edward626kr@gmail.com>
2024-04-03 09:25:49 +0900

    Fix: 카드 디테일 전체 클릭하면 갈 수 있도록
    Change-Id: I3eda522403f1daa1dc323fb185244ac97362cc6f
    
    Former-commit-id: e11d791c4375027840a7fb588586ad7d90d6c9e0

--- frontend/src/pages/CardPage/CardDetailPage.jsx
+++ frontend/src/pages/CardPage/CardDetailPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ad58790f411bf3e6bcb593c8a79ce727bc3debb557c3809bf5b6475a8291f21
-size 10663
+oid sha256:4a471e2330b5036d5b5c6ca8ff9673df73e18c99daf0d2759ca168d99fb111f8
+size 10665
