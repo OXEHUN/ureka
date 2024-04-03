e9b39c476748e699d493a8f635db3ef46da6e956
최용훈 <cyh2607@gmail.com>
2024-04-04 01:37:03 +0900

    FIX:다음월로 이동 막음
    Change-Id: I2cb7227dcb839c1cab9d9782c3e7867609ce15a2
    
    Former-commit-id: 0ca63a13e5c1417f72e84a63cda4b3e816dc04aa

--- frontend/src/pages/CardPage/CardDetailPage.jsx
+++ frontend/src/pages/CardPage/CardDetailPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6fe772e9c4f9b3fd4e4edf8e667a0d4a34a6e9da6338e1ca49587a3032bce50
-size 10905
+oid sha256:8a2debe92c34fa0c85cbe755a946e1668b8b55cbdbe10041f97d7eaaf4109d61
+size 11270
