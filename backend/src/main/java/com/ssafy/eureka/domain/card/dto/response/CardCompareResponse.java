6df0a0fbcc36883425f6ef5bb5e337e63e3a71f7
kdw <hide4321@naver.com>
2024-04-03 09:44:51 +0900

    Fix: top3 추천 카드 cardId 추가, 카드 비교 또래 카드 추가 중
    Change-Id: I6b451e2c661ed600ed3bd90be66eae83fc875a82
    
    Former-commit-id: 1dbf96deb9aaa488a11262b42a8400ddf3076322

--- backend/src/main/java/com/ssafy/eureka/domain/card/dto/response/CardCompareResponse.java
+++ backend/src/main/java/com/ssafy/eureka/domain/card/dto/response/CardCompareResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a7cda81b8639029ab1c3671411fc6388cb1051c5dbf7099235ac1c4623d65e7
-size 2494
+oid sha256:1819caec840008d4eb8fd96eff09a6b2aabe682eeb5b7b5f6585fa010744c50f
+size 2431
