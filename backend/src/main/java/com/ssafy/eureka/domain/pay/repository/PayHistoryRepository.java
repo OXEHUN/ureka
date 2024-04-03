59c53e11065cab34c5e543dedf99efb0ec34036e
김해인 <khi762@naver.com>
2024-04-03 21:47:23 +0900

    Fix: 홈화면 페이 결제 요청 수정
    Change-Id: I8ce8b11c0f1bcecf7b7ec8c5bb4762076f408185
    
    Former-commit-id: 9c0e356590831cd93b4031050d4fdd99ba9e6316

--- backend/src/main/java/com/ssafy/eureka/domain/pay/repository/PayHistoryRepository.java
+++ backend/src/main/java/com/ssafy/eureka/domain/pay/repository/PayHistoryRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9dbb226536426f65f7dc352b9720d6cb921571fce9141c505af971833c25485e
-size 2168
+oid sha256:bb92886a453c1934c69226725deb13b0c1a35bfffe204d564e4925da08d73d81
+size 2258
--- frontend/src/pages/HomePage/Compare.jsx
+++ frontend/src/pages/HomePage/Compare.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6df510efba2f5ec1fe0a82855296849d749e6e33d18ec0154d7f62dce50e3b2c
-size 8004
+oid sha256:2dc66b948c9ca5d0819a7db10de3034873ac35b47b42e7ebdf140f39e17feb61
+size 7972
