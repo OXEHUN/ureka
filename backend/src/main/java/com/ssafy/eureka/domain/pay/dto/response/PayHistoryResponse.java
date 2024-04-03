c2f1528cc7aa8aab6b63924978e5d0f4f389d4af
yeongdae <oooo_0@naver.com>
2024-04-03 14:48:37 +0900

    Fix: 페이 결제 내역 오류 수정
    Change-Id: Icecc21e9a40f4929060e03612199d1f53ae89a1f
    
    Former-commit-id: 49bd8b2e30a6fd99244a683dee4a5e9b672a9bb9

--- backend/src/main/java/com/ssafy/eureka/domain/pay/dto/response/PayHistoryResponse.java
+++ backend/src/main/java/com/ssafy/eureka/domain/pay/dto/response/PayHistoryResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cbd1263a30ac1237470fb9cbbb7cb7e8fd2eefe892d154b36f50d824f433beea
-size 576
+oid sha256:1adca65dcdf6ca42201f9bb42449ba3393d4ea4a03dc51d84116dc3f54c21e7d
+size 605
--- backend/src/main/java/com/ssafy/eureka/domain/pay/service/PayServiceImpl.java
+++ backend/src/main/java/com/ssafy/eureka/domain/pay/service/PayServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8826739fb18ff302b88be5bbd9553274bd18eb9ece71de412c28cb30c23032b4
-size 20626
+oid sha256:68de2b9f74224435b4485a6e51472c65acf91f8e48bf0cdc0d2fbc5fdb31e693
+size 18438
