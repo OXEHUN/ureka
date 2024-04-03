4d2f603fb263ef44309fd6ca6eb367b7bcd647c6
kdw <hide4321@naver.com>
2024-04-03 23:03:08 +0900

    Fix: CardOwnershipStaticRepository 쿼리 수정
    Change-Id: I79d8c2078d62da0b4fb7ed4b49f5ff030af0a376
    
    Former-commit-id: 81057587b91e227696e04acb4dd5e2322d825e05

--- backend/src/main/java/com/ssafy/eureka/domain/statistics/service/StatisticServiceImpl.java
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/service/StatisticServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c59284df30fd3253bfd902b15fd271c8450527a239d7e9f57f44d820e32c8188
-size 18621
+oid sha256:f3fe7f0fe8a025a106b58fae358b255cedb7b27cee5e1275a63b761a657a7dcb
+size 19072
