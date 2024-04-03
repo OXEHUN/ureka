706976c4507c06837c9634785e1132533d31ef71
yeongdae <oooo_0@naver.com>
2024-04-03 16:54:36 +0900

    Fix: 카드 비교 DTO 수정
    Change-Id: Iae9892da18c814b7994cbdf6f293e4429035eb63
    
    Former-commit-id: 147c1075579616f6532bb3a49eb5ed5be4cb9e09

--- backend/src/main/java/com/ssafy/eureka/domain/statistics/dto/CardOwnershipDto.java
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/dto/CardOwnershipDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c44b2c67a36ff88b28aa917ad2b9ffcf44054821e53e75570eedbad1d62482b
-size 2110
+oid sha256:d7985b1eeb9dcbf4531f589ea1b51a8954c9a736389dc1b9a2e0ace62036d816
+size 2126
