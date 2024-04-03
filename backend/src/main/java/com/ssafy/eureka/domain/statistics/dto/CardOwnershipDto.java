36cd62b3880bd8057f31a79ac6a9b14f40343422
yeongdae <oooo_0@naver.com>
2024-04-03 16:53:54 +0900

    Fix: 카드 조회 에러 수정
    Change-Id: I9e2711ebd6aa3f77d6f0fe16e879290f436494f4
    
    Former-commit-id: b5b034c0890f0582c2b3353101ed1e4db27ee274

--- backend/src/main/java/com/ssafy/eureka/domain/statistics/dto/CardOwnershipDto.java
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/dto/CardOwnershipDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4c257e4d107bf05c9058d2f05df64b4c9a46a27ea34dcc74a500cd7890b600f
-size 2049
+oid sha256:f4b21d3d9e6e44d2b993c34df52b85ae1e4f9dd4e81a3a8d7b4134ba96218a32
+size 2046
