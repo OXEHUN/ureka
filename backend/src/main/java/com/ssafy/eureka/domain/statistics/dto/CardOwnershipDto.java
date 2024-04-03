ca9b26e12f693d46ece6abb68b7549efde995e12
김해인 <khi762@naver.com>
2024-04-03 16:59:23 +0900

    Fix: compare err fix
    Change-Id: I5fc9a9b81450460035476f85d675cceb8a9b8d67
    
    Former-commit-id: f6577b4418063c64cc9aad67b097ef41bfddd95d

--- backend/src/main/java/com/ssafy/eureka/domain/statistics/dto/CardOwnershipDto.java
+++ backend/src/main/java/com/ssafy/eureka/domain/statistics/dto/CardOwnershipDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c44b2c67a36ff88b28aa917ad2b9ffcf44054821e53e75570eedbad1d62482b
-size 2110
+oid sha256:d3084dcc0303809804050be86735ccdb62b54a114405d9cc895b286507b3e84e
+size 2123
