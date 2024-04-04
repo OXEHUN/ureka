a3c98dcdf5129fc99c4ee0d285d57ca5e716c241
oksehun <sehun9803@naver.com>
2024-04-04 10:14:18 +0900

    Fix: 카상추
    Change-Id: I329fa287289f26d4b68c0d0233aabc9d93afff8a
    
    Former-commit-id: 102bfeb4b005cba3b8f8f344c9a5c8bda282ece2

--- frontend/src/pages/ProductPage/components/CompareCard.jsx
+++ frontend/src/pages/ProductPage/components/CompareCard.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8fb67fe813007902f39e800a70a63ecf28dbe37f5d1d940f5f0e3ed53231929
-size 20515
+oid sha256:ad500e80dfc05eb3a70d4af02307c3635dbfe9f26a892451ecff1268ccec4568
+size 21484
