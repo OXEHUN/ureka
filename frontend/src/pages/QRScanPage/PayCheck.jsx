51c8feb67446f4fed3c4e08077a5566d8cda5f11
김해인 <khi762@naver.com>
2024-04-04 10:58:16 +0900

    Fix: 페이 결제 할인금액 추가
    Change-Id: Ic5bc85710a6f321022cbe5ad32a1c173ab5ec73b
    
    Former-commit-id: 3e61686ec05875901d740f11eb338413219d243f

--- frontend/src/pages/QRScanPage/PayCheck.jsx
+++ frontend/src/pages/QRScanPage/PayCheck.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30e81fc6329b0d6ec1994417ce95e8a73545515596b10a542eb97045063484c2
-size 11439
+oid sha256:6dda69a686c50863cafa5edd1519bef90aa98aecc3e340ec7842a6aadfe7b63f
+size 11602
