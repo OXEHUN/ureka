eb40a6fafed71b63d174ab85b5bcb66e73665899
oksehun <sehun9803@naver.com>
2024-04-04 12:21:34 +0900

    설정버튼 수정
    Change-Id: I3b793d454a816e5a5d5b2884732c8ebaf4728e88
    
    Former-commit-id: 8493d725022c1f65ac0d948f91f0820ac193f3c9

--- frontend/src/pages/HomePage/components/HomePayCard.jsx
+++ frontend/src/pages/HomePage/components/HomePayCard.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2fc60e42d52e7e028fba691e361a949309bc2cefdfc6bd70a039655d7b234b8
-size 1714
+oid sha256:24f7f507204c1019d707a8a61678f38560d1886ab5a194792f0f276c13dcb64e
+size 1968
