e26a0a5c6dfee194fe7a427d4938b68bc15b0230
oksehun <sehun9803@naver.com>
2024-04-04 09:55:02 +0900

    Fix: 홈화면
    Change-Id: I0517911cb7a93653900e6927eedd889f05026e22
    
    Former-commit-id: 2bda258888d2419e9cb8be630de0d423f20c490d

--- frontend/src/pages/HomePage/components/HomePayCard.jsx
+++ frontend/src/pages/HomePage/components/HomePayCard.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:955111c83e91da10ea45856a7c4d70b7917bcfa3f7df6ccf4fb56cc8860f0d69
-size 1386
+oid sha256:a2fc60e42d52e7e028fba691e361a949309bc2cefdfc6bd70a039655d7b234b8
+size 1714
