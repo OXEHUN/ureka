b28bb307533a902c383f6e9f91990385ca9e9017
yeongdae <oooo_0@naver.com>
2024-04-04 09:29:57 +0900

    Fix: 상위 카테고리 추천 할인 값 오류 수정
    Change-Id: I64bbab6f3402d45011ce97ca03834d35478412ef
    
    Former-commit-id: 636ce48fbf99ae409be7320aa0a305e88ff2f8b4

--- document/sql/eureka_sql/eureka_db_dummy_1.sql
+++ document/sql/eureka_sql/eureka_db_dummy_1.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e028074bbb6c5f753f98c8df73d92419a5e87099ebb5d69871d83594e4e1674a
-size 9923
+oid sha256:db7ffb27623d70f8e06568e251292ba97ebedab9333d908847706a6cda9386f5
+size 10683
