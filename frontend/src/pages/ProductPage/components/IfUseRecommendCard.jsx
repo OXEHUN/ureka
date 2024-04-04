10f5449a03731261a98fc48523e0f404f7046494
oksehun <sehun9803@naver.com>
2024-04-04 10:14:18 +0900

    Fix: 카상추
    Change-Id: I329fa287289f26d4b68c0d0233aabc9d93afff8a
    
    Former-commit-id: fdb078f2799002e583e7780bbcc985084b1162df

--- frontend/src/pages/ProductPage/components/IfUseRecommendCard.jsx
+++ frontend/src/pages/ProductPage/components/IfUseRecommendCard.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3cfb6e5e67e9ab2d70e865e54a2dfda12a11a537d73de7584bbb6f9125b6bf8
-size 8966
+oid sha256:d444281764e9e09fae6a3ec08ae9017b5b629c8fb53a649ad9b098495b1d06c4
+size 8897
