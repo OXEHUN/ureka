c7e4980cddbf4b5e0e42a9baa4323342102a0b9a
kdw <hide4321@naver.com>
2024-04-23 12:06:43 +0900

    Fix: ReadME.md 크기 수정
    Change-Id: I4bb2a393e2e7f505ed814a432c82f2d15e3b41c3
    
    Former-commit-id: 72fa0478d69480bb9e50581384b79cb1a297de32

--- backend/src/main/java/com/ssafy/eureka/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/eureka/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0434b7590dd220ca316e7ddcdfe5273660fab4b75c0175fd424789fe3a4239ff
+oid sha256:dbf0036539a93b81f4f22d69a46a9a2d02d89e30e910343031383f8d5d9a4bcc
 size 3040
